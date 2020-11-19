7e0f7509bb64c84f185d8fc4b97db36ba483395f
Alejandro Ortiz <cristian160595@gmail.com>
2020-11-19 16:27:42 -0600

    se  instalo firebase en el proyecto y se agregarón las variables de configuarión para realizar el deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBkpGf2beX9HrmX9hKyzVF_wgbbchpkSE0',
+    authDomain: 'business-manager-c488c.firebaseapp.com',
+    databaseURL: 'https://business-manager-c488c.firebaseio.com',
+    projectId: 'business-manager-c488c',
+    storageBucket: 'business-manager-c488c.appspot.com',
+    messagingSenderId: '332785373626',
+    appId: '1:332785373626:web:9636ad5c643bd3b9c0cd83',
+    measurementId: 'G-H7T0EBKV8Q'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBkpGf2beX9HrmX9hKyzVF_wgbbchpkSE0',
+    authDomain: 'business-manager-c488c.firebaseapp.com',
+    databaseURL: 'https://business-manager-c488c.firebaseio.com',
+    projectId: 'business-manager-c488c',
+    storageBucket: 'business-manager-c488c.appspot.com',
+    messagingSenderId: '332785373626',
+    appId: '1:332785373626:web:9636ad5c643bd3b9c0cd83',
+    measurementId: 'G-H7T0EBKV8Q'
+  }
 };
 
 /*
