d376a946099e136b4f91348b6e6622c7fdc88412
rubpu <rubpuga@gmail.com>
2021-05-25 12:04:40 -0500

    added contract modal options

---
+++ src/app/pages/contracts/components/contract-modal-options/contract-modal-options.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-modal-options',
+  templateUrl: './contract-modal-options.component.html',
+  styleUrls: ['./contract-modal-options.component.scss'],
+})
+export class ContractModalOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
