7c5f215a35560d7f099a9b06534d3d0c2671544c
rubpu <rubpuga@gmail.com>
2021-06-18 09:00:07 -0500

    Added ticket table to contract info page

--- src/app/pages/contracts/contract-info/components/tickets-table/tickets-table.component.ts
+++ src/app/pages/contracts/contract-info/components/tickets-table/tickets-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import { MatTable } from '@angular/material/table';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tickets-table',
@@ -7,8 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketsTableComponent implements OnInit {
 
+  @Input() ticketRefList: DocumentReference[];
+  public ticketList: any[] = [];
+  public displayColumns: string[] = 
+    ['id', 'date', 'gross', 'tare', 'net', 'opt'];
+  @ViewChild(MatTable) table: MatTable<any>
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.ticketRefList.forEach(ticketRef => {
+      let counter = 0;
 
+      ticketRef.get().then(val => {
+        this.ticketList.push(val.data());
+
+        if(this.ticketList.length == this.ticketRefList.length) {
+          this.table.renderRows();
+          console.log(this.table);
+          console.log("Ticket table done");
+        }
+      })
+    })
+  }
 }
