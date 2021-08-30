89164f94fe7f68f90698b08097a5018006d6e3d9
Ruben Puga <rubpuga@gmail.com>
2021-08-30 07:10:06 -0500

    dicount table

--- src/app/pages/inventory/product/components/discount-table/discount-table.component.ts
+++ src/app/pages/inventory/product/components/discount-table/discount-table.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AngularFirestore, DocumentSnapshot } from '@angular/fire/firestore';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, DocumentSnapshot } from '@angular/fire/firestore';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTable } from '@angular/material/table';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-discount-table',
@@ -9,27 +11,57 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DiscountTableComponent implements OnInit {
   @Input() doc: DocumentSnapshot<any>;
+  @ViewChild(MatTable) tableView:MatTable<any>;
 
+  public ready = false;
   public table: FormGroup;
+  public currentCompany: string;
+  public displayedColumns: string[] = ['low', 'high', 'weightDiscount', 'charge', 'bonus'];
 
   constructor(
     private db: AngularFirestore,
     private fb: FormBuilder,
+    private storage: Storage
   ) { }
 
   ngOnInit() {
     this.table = this.fb.group({
       data: this.fb.array([])
+    });
+    
+    new AngularFirestoreDocument(this.doc.ref, this.db).collection("discounts").doc("moisture").get().subscribe(val => {
+      if(val.data['data']){
+        val.data['data'].forEach(row => {
+          this.data.push(this.createItem(row));
+        });  
+      }
+      
+      if (this.data.length == 0) {
+        this.createItem();
+      }
+
+      this.ready = true;
+      this.tableView.renderRows();
     })
   }
 
-  createItem(): FormGroup{
+  createItem(row?: any): FormGroup{
+    if(typeof row == "undefined"){
+      row = {
+        low: null,
+        high: null,
+        weightDiscount: null,
+        charge: null,
+        bonus: null
+      }
+    }
+
     return this.fb.group({
-      low: [,Validators.required],
-      high: [,Validators.required],
-      weightDiscount: [,Validators.required],
-      charge: [,Validators.required],
-      bonus: [,Validators.required]
+      low: [row.low,Validators.required],
+      high: [row.high,Validators.required],
+      weightDiscount: [row.weightDiscount,Validators.required],
+      charge: [row.charge,Validators.required],
+      bonus: [row.bonus,Validators.required]
     })
   }
 
--- src/app/pages/inventory/product/product.page.ts
+++ src/app/pages/inventory/product/product.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, DocumentSnapshot } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-product',
@@ -9,15 +10,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductPage implements OnInit {
   public product:string;
+  public currentCompany:string;
+  public ready:boolean = false;
+  public doc: DocumentSnapshot<any>;
 
   constructor(
     private route: ActivatedRoute,
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private storage: Storage
   ) { 
     this.product = route.snapshot.paramMap.get('product');
   }
 
   ngOnInit() {
+    this.storage.get("currentCompany").then(company => {
+      this.currentCompany = company;
+
+      this.db.doc<any>(`companies/${company}/products/${this.product}`).get().subscribe(val => {
+        this.doc = val;
+        this.ready = true;
+      })
+    })
   }
 
 }
