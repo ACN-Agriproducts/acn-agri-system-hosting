ae59fd242c6ac7c4ddc3be1786a68d9b6cee928d
rubpu <rubpuga@gmail.com>
2021-09-10 14:49:04 -0500

    Added DPR ticket table component

---
+++ src/app/pages/inventory/product/components/dpr-tickets-table/dpr-tickets-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dpr-tickets-table',
+  templateUrl: './dpr-tickets-table.component.html',
+  styleUrls: ['./dpr-tickets-table.component.scss'],
+})
+export class DprTicketsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
