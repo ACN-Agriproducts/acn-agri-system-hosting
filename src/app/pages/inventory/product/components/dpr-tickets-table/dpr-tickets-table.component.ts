93a4b16802d335e1955636048df982ff05af5de6
rubpu <rubpuga@gmail.com>
2021-09-10 15:48:05 -0500

    Finished DPR Ticket Tables

--- src/app/pages/inventory/product/components/dpr-tickets-table/dpr-tickets-table.component.ts
+++ src/app/pages/inventory/product/components/dpr-tickets-table/dpr-tickets-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DocumentReference } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dpr-tickets-table',
@@ -6,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dpr-tickets-table.component.scss'],
 })
 export class DprTicketsTableComponent implements OnInit {
+  @Input() ticketList: any[] = [];
+  @Input() ticketType: string = "";
+  @Input() bushelWeight: number = 1;
+  public ticketData: any[];
 
-  constructor() { }
+  public displayedColumns: string[] = ['id', 'client', 'net']
+  
+  private currentSub: Subscription[] = [];
+  public ready: boolean = false;
 
-  ngOnInit() {}
+  constructor() {}
 
+  ngOnInit() {
+    this.ticketData = new Array<any>(this.ticketList.length);
+    let counter = 0;
+
+    this.ticketList.forEach((ticket, index) => {
+      let ticketRef = ticket as DocumentReference<any>;
+
+      ticketRef.get().then(val => {
+        this.ticketData[index] = val.data();
+        counter++;
+
+        if(counter == this.ticketData.length){
+          this.ready = true;
+        }
+      });
+    })}
+
+  getTotalWeight():number {
+    let counter = 0;
+
+    for(const x of this.ticketData) {
+      counter += x.dryWeight;
+    }
+    return counter;
+  }
 }
