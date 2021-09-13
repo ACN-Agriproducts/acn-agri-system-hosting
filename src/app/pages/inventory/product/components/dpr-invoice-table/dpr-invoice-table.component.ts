8ecdeff815db05e4ec2d7bbaf490e8d426dd8641
rubpu <rubpuga@gmail.com>
2021-09-13 08:43:00 -0500

    Add invoice DPR table

---
+++ src/app/pages/inventory/product/components/dpr-invoice-table/dpr-invoice-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dpr-invoice-table',
+  templateUrl: './dpr-invoice-table.component.html',
+  styleUrls: ['./dpr-invoice-table.component.scss'],
+})
+export class DprInvoiceTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
