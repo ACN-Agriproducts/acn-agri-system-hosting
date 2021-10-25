7b08192dd84b4a96a11bf8a7500d25c54a951417
rubpu <rubpuga@gmail.com>
2021-10-25 09:14:55 -0500

    Finished printable liquidation

--- src/app/shared/contract-liquidation-long/contract-liquidation-long.component.ts
+++ src/app/shared/contract-liquidation-long/contract-liquidation-long.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contract-liquidation-long',
@@ -6,9 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-liquidation-long.component.scss'],
 })
 export class ContractLiquidationLongComponent implements OnInit {
+  public ticketList: any[];
+  @Input() contract: any;
+
+  public gross: number = 0;
+  public tare: number = 0;
+  public moistureDiscount: number = 0;
+  public moistureAdjustedWeight: number = 0;
+  public infestedTotal: number = 0;
+  public inspectionTotal: number = 0;
+  public netToPayTotal: number = 0;
 
   constructor() { }
 
   ngOnInit() {}
 
+  public setTickets(tempList: any[]): void {
+    this.ticketList = tempList;
+
+    this.ticketList.forEach(ticket => {
+      this.gross += ticket.gross;
+      this.tare += ticket.tare;
+      this.moistureDiscount += ticket.dryWeight - (ticket.gross - ticket.tare);
+      this.moistureAdjustedWeight += ticket.dryWeight;
+    });
+  }
+
+  public getDate(): Date {
+    return new Date();
+  }
 }
