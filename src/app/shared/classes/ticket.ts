c23ce2c4b3a5051af5b3d4c4fa2cec468a2fac5b
rubpu <rubpuga@gmail.com>
2022-02-23 10:32:22 -0600

    Added ticket class

---
+++ src/app/shared/classes/ticket.ts
@@ -0,0 +1,108 @@
+import { DocumentData, DocumentReference, QueryDocumentSnapshot, SnapshotOptions } from "@angular/fire/compat/firestore";
+
+export class Ticket {
+    public clientName: string;
+    public comment: string;
+    public contractID: number;
+    public dateIn: Date;
+    public dateOut: Date;
+    public discount: number;
+    public driver: string;
+    public dryWeight: number;
+    public deryWeightPercent: number;
+    public grade: number;
+    public gross: number;
+    public id: number;
+    public imageLinks: string[];
+    public in: boolean;
+    public moisture: number;
+    public origin: string;
+    public original_ticket: string;
+    public original_weight: number;
+    public pdfLink: string;
+    public plague: string;
+    public plates: string;
+    public PPB: number;
+    public productName: string;
+    public tank: string;
+    public tankId: number;
+    public tare: number;
+    public truckerId: string;
+    public vehicleID: string;
+    public weight: number;
+
+    public dataPath: DocumentReference;
+
+    constructor(snapshot: QueryDocumentSnapshot<any>) {
+        const data = snapshot.data();
+
+        this.clientName = data.clientName;
+        this.comment = data.comment;
+        this.contractID = data.contractID;
+        this.dateIn = data.dateIn;
+        this.dateOut = data.dateOut;
+        this.discount = data.discount;
+        this.driver = data.driver;
+        this.dryWeight = data.dryWeight;
+        this.deryWeightPercent = data.deryWeightPercent;
+        this.grade = data.grade;
+        this.gross = data.gross;
+        this.id = data.id;
+        this.imageLinks = data.imageLinks;
+        this.in = data.in;
+        this.moisture = data.moisture;
+        this.origin = data.origin;
+        this.original_ticket = data.original_ticket;
+        this.original_weight = data.original_weight;
+        this.pdfLink = data.pdfLink;
+        this.plague = data.plague;
+        this.plates = data.plates;
+        this.PPB = data.PPB;
+        this.productName = data.productName;
+        this.tank = data.tank;
+        this.tankId = data.tankId;
+        this.tare = data.tare;
+        this.truckerId = data.truckerId;
+        this.vehicleID = data.vehicleID;
+        this.weight = data.weight;
+    }
+    // Converters
+    public static ticketConverter = {
+        toFirestore(data: Ticket): DocumentData {
+            return {
+                clientName: data.clientName,
+                comment: data.comment,
+                contractID: data.contractID,
+                dateIn: data.dateIn,
+                dateOut: data.dateOut,
+                discount: data.discount,
+                driver: data.driver,
+                dryWeight: data.dryWeight,
+                deryWeightPercent: data.deryWeightPercent,
+                grade: data.grade,
+                gross: data.gross,
+                id: data.id,
+                imageLinks: data.imageLinks,
+                in: data.in,
+                moisture: data.moisture,
+                origin: data.origin,
+                original_ticket: data.original_ticket,
+                original_weight: data.original_weight,
+                pdfLink: data.pdfLink,
+                plague: data.plague,
+                plates: data.plates,
+                PPB: data.PPB,
+                productName: data.productName,
+                tank: data.tank,
+                tankId: data.tankId,
+                tare: data.tare,
+                truckerId: data.truckerId,
+                vehicleID: data.vehicleID,
+                weight: data.weight
+            }
+        },
+        fromFirestore(snapshot: QueryDocumentSnapshot<any>, options: SnapshotOptions): Ticket {
+            return new Ticket(snapshot);
+        }
+    }
+}
