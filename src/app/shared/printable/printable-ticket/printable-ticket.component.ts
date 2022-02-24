a41bcef5bf371841e434ee915266c42ab54da3c5
Pugster01 <rubpuga@gmail.com>
2022-02-24 16:35:43 -0600

    Changed printable-ticket types

--- src/app/shared/printable/printable-ticket/printable-ticket.component.ts
+++ src/app/shared/printable/printable-ticket/printable-ticket.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Contact } from '@shared/classes/contact';
+import { Contract } from '@shared/classes/contract';
+import { Ticket } from '@shared/classes/ticket';
 
 
 @Component({
@@ -7,10 +10,10 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./printable-ticket.component.scss']
 })
 export class PrintableTicketComponent implements OnInit {
-  @Input() ticket: any;
-  @Input() contract: any;
-  @Input() transport: any;
-  @Input() client: any;
+  @Input() ticket: Ticket;
+  @Input() contract: Contract;
+  @Input() transport: Contact;
+  @Input() client: Contact;
   public valuesList: any[] = [{label: 'test', value: 'test' }];
 
   constructor() {}
@@ -29,7 +32,7 @@ export class PrintableTicketComponent implements OnInit {
       {label: 'Origin', value: this.ticket.origin},
       {label: 'DryWeight', value: this.ticket.dryWeight},
       {label: 'Orig. Ticket #', value: this.ticket.original_ticket},
-      {label: 'Discount/100lbs', value: this.ticket.dryWeightPercent * 100},
+      {label: 'Discount/100lbs', value: (this.ticket.dryWeight / this.ticket.getNet() * 1000) % 1 / 10},
       {label: 'Orig Weight', value: this.ticket.original_weight},
       {label: 'DryWeight', value: 54780},
       {label: 'Tank', value: 'Tank 5'},
