b607bcd7d4922f20f9d705a5c7172a1b0f8c1413
rubpu <rubpuga@gmail.com>
2022-03-11 10:27:28 -0600

    Added ticket-discount-table component

---
+++ src/app/pages/contracts/contract-info/components/ticket-discount-table/ticket-discount-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-discount-table',
+  templateUrl: './ticket-discount-table.component.html',
+  styleUrls: ['./ticket-discount-table.component.scss'],
+})
+export class TicketDiscountTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
