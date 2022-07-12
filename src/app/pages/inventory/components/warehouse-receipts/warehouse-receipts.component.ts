8a64314735979c037382a6369a3fc87e4e2e43f5
Jeddie <eduardoreyna0625@hotmail.com>
2022-07-12 14:20:03 -0500

    Generated warehouse receipt component

---
+++ src/app/pages/inventory/components/warehouse-receipts/warehouse-receipts.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-receipts',
+  templateUrl: './warehouse-receipts.component.html',
+  styleUrls: ['./warehouse-receipts.component.scss'],
+})
+export class WarehouseReceiptsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
