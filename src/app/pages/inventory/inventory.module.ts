99f0c7a8804621085165e2807ff9429d006c8a9a
Jeddie <eduardoreyna0625@hotmail.com>
2022-07-25 10:05:53 -0500

    Finished refactoring for warehouse receipts page

--- src/app/pages/inventory/inventory.module.ts
+++ src/app/pages/inventory/inventory.module.ts
@@ -13,9 +13,7 @@ import { StoragePopoverComponent } from './components/storage-popover/storage-po
 import { MoveInvDialogComponent } from './components/storage-popover/dialogs/move-inv-dialog/move-inv-dialog.component';
 import { EditInvDialogComponent } from './components/storage-popover/dialogs/edit-inv-dialog/edit-inv-dialog.component';
 import { ZeroOutTankDialogComponent } from './components/storage-popover/dialogs/zero-out-tank-dialog/zero-out-tank-dialog.component';
-import { WarehouseReceiptsComponent } from './components/warehouse-receipts/warehouse-receipts.component';
-import { NewWarehouseReceiptModalComponent } from './components/new-warehouse-receipt-modal/new-warehouse-receipt-modal.component';
-import { WarehouseReceiptStatusPopoverComponent } from './components/warehouse-receipt-status-popover/warehouse-receipt-status-popover.component';
+
 
 @NgModule({
   imports: [
@@ -33,9 +31,6 @@ import { WarehouseReceiptStatusPopoverComponent } from './components/warehouse-r
     MoveInvDialogComponent,
     EditInvDialogComponent,
     ZeroOutTankDialogComponent,
-    WarehouseReceiptsComponent,
-    NewWarehouseReceiptModalComponent,
-    WarehouseReceiptStatusPopoverComponent,
   ],
 })
 export class InventoryPageModule {}
