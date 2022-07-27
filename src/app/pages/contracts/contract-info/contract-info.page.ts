70ef6a5d91bbc6161350ef9cf1240e77fcc2e594
Jesus Eduardo Reyna <44928860+JeddieYayo@users.noreply.github.com>
2022-07-27 11:55:20 -0500

    Jeddie contract liquidation download (#48)
    * Created new version of onDownloadLiquidation
    
    * Added new ticket Report type and column
    
    * Refactored liquidation function to use exceljs
    
    * Making contract liquidation more readable
    
    Co-authored-by: rubpu <rubpuga@gmail.com>
    Co-authored-by: Pugster01 <43832406+Pugster01@users.noreply.github.com>

--- src/app/pages/contracts/contract-info/contract-info.page.ts
+++ src/app/pages/contracts/contract-info/contract-info.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { Subscription } from 'rxjs';
@@ -10,6 +10,7 @@ import { utils, WorkBook, writeFile } from 'xlsx';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import * as Excel from 'exceljs';
 
 @Component({
   selector: 'app-contract-info',
@@ -28,6 +29,7 @@ export class ContractInfoPage implements OnInit, OnDestroy {
   public ticketsReady: boolean = false;
   public contractRef: AngularFirestoreDocument;
   public showLiquidation: boolean = false;
+
   private currentSub: Subscription;
 
   @ViewChild(ContractLiquidationLongComponent) printableLiquidation: ContractLiquidationLongComponent;
@@ -67,16 +69,136 @@ export class ContractInfoPage implements OnInit, OnDestroy {
     }
   }
 
-  onDownloadLiquidation() {
-    const table = this.printableLiquidation.getTable();
-    const workBook:WorkBook = utils.table_to_book(table);
+  public onDownloadLiquidation = async () => {
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet();
 
-    writeFile(workBook, `contract-${this.currentContract.id}-liquidation.xlsx`);
+    // initializing columns
+    worksheet.columns = [
+      { header: "Inbound Date", key: 'dateIn' },
+      { header: "Ticket #", key: 'id' },
+      { header: "Contract", key: 'contractID' },
+      { header: "Gross", key: 'gross' },
+      { header: "Tare", key: 'tare' },
+      { header: "Net", key: 'net' },
+      { header: "%", key: 'moisture' },
+      { header: "CWT", key: 'moistureCwt' },                        // net - dryWeight
+      { header: "Adjusted Weight", key: 'dryWeight' },
+      { header: "%", key: 'damage' },                               // not in the ticket yet
+      { header: "CWT", key: 'damageCwt' },                          // damageWeight - undamagedWeight
+      { header: "Adjusted Weight", key: 'undamagedWeight' },
+      { header: "Price ($/BU)", key: 'pricePerBushel' },            // located in contract
+      { header: "Adjusted Price ($/BU)", key: 'adjustedPrice' },    // ???
+      { header: "Total ($)", key: 'total' },                        // AdjustedWeight / Product.weight * price
+      { header: "Infested", key: 'infested' },      
+      { header: "Inspection", key: 'inspection' },
+      { header: "Net to Pay ($)", key: 'netToPay' },                // total - infested - inspection
+    ];
+
+    let rowValues = [];
+    rowValues[4] = "Weight(lbs)";
+    rowValues[7] = "Moisture";
+    rowValues[10] = "Damage";
+    let firstRow = worksheet.insertRow(1, rowValues);
+
+    // formatting
+    worksheet.mergeCells('D1:F1');
+    worksheet.mergeCells('G1:H1');
+    worksheet.mergeCells('J1:K1');
+
+    firstRow.alignment = { horizontal: 'center' };
+    firstRow.font = { bold: true };
+    worksheet.getRow(2).font = { bold: true };
+    worksheet.getRow(2).border = { bottom: { style: 'thick'}};
+
+    worksheet.columns.forEach(column => {
+      const lengths = column.values.map(v => v.toString().length);
+      const maxLength = Math.max(...lengths.filter(v => typeof v === 'number'));
+      column.width = maxLength;
+    });
+
+    // populating worksheet columns
+    this.ticketDiscountList.forEach(ticket => {
+      const net = ticket.data.getNet() * (ticket.data.in ? 1 : -1);
+
+      const dryWeight = ticket.data.dryWeight;
+      const moistureCwt = net - dryWeight;
+
+      const undamagedWeight = dryWeight;                // remove later when damage/undamagedWeight is added to tickets (ticket.data.undamagedWeight)
+      const damageCwt = dryWeight - undamagedWeight;
+      
+      worksheet.addRow({
+        ...ticket.data,
+        net: net,
+        moisture: ticket.data.moisture,
+        moistureCwt: moistureCwt,
+        damage: 0,                                      // change later when damage/undamagedWeight is added to tickets
+        damageCwt: damageCwt,
+        undamagedWeight: undamagedWeight,               // change later when damage/undamagedWeight is added to tickets
+        pricePerBushel: this.currentContract.pricePerBushel,
+        adjustedPrice: this.currentContract.pricePerBushel,
+        total: this.currentContract.pricePerBushel * undamagedWeight / 56,
+        infested: ticket.discounts.infested,
+        inspection: ticket.discounts.inspection,
+        netToPay: this.currentContract.pricePerBushel / 56 * dryWeight - ticket.discounts.infested - ticket.discounts.inspection
+      });
+    });
+
+    // adding totals row
+    const colNamesArray = [
+      'gross',
+      'tare',
+      'net',
+      'moistureCwt',
+      'dryWeight',
+      'damageCwt',
+      'undamagedWeight',
+      'pricePerBushel',
+      'adjustedPrice',
+      'total',
+      'infested',
+      'inspection',
+      'netToPay'
+    ];
+    this.addColumnTotal(worksheet, colNamesArray);
+
+    this.toDownload(workbook);
   }
 
-  reloadContractTickets() {
-    
+  public addColumnTotal = (worksheet: Excel.Worksheet, colKeys: string[]) => {
+    const totalRow = worksheet.addRow(["Totals:"]);
+    const row = totalRow.number;
 
+    colKeys.forEach(colKey => {
+      const col = String.fromCharCode('A'.charCodeAt(0) + worksheet.getColumn(colKey).number - 1);
+
+      totalRow.getCell(colKey).value = { 
+        formula: `SUM(${col}3:${col}${row - 1})`
+      } as Excel.CellFormulaValue;
+    });
+    totalRow.font = { bold: true };
+    totalRow.border = { top: { style: 'thick' } };
+  }
+  
+  public toDownload = async (workbook: Excel.Workbook) => {
+    const buffer = await workbook.xlsx.writeBuffer();   
+    const blob = new Blob([buffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    });
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+
+    a.setAttribute('style', 'display: none');
+    a.href = url;
+    a.download = `contract-${this.currentContract.id}-liquidation.xlsx`;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  } 
+
+  reloadContractTickets() {
     this.fns.httpsCallable('contracts-updateTickets')({
       company: this.currentCompany,
       contractId: this.id,
