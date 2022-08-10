c0861f18b58dfe6921402f116247edab796481dc
rubpu <rubpuga@gmail.com>
2022-08-10 11:45:35 -0500

    Added dashboard Components

---
+++ src/app/pages/home/components/needs-admin-attention/needs-admin-attention.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-needs-admin-attention',
+  templateUrl: './needs-admin-attention.component.html',
+  styleUrls: ['./needs-admin-attention.component.scss'],
+})
+export class NeedsAdminAttentionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
