dbe09e13fe75e0a8d0bb1122d806c29f54a6eb7c
rubpu <rubpuga@gmail.com>
2022-08-10 16:19:06 -0500

    Get tickets from database

--- src/app/pages/home/components/needs-admin-attention/needs-admin-attention.component.ts
+++ src/app/pages/home/components/needs-admin-attention/needs-admin-attention.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Firestore, where } from '@angular/fire/firestore';
+import { Storage } from '@ionic/storage';
+import { Plant } from '@shared/classes/plant';
+import { Ticket } from '@shared/classes/ticket';
 
 @Component({
   selector: 'app-needs-admin-attention',
@@ -6,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./needs-admin-attention.component.scss'],
 })
 export class NeedsAdminAttentionComponent implements OnInit {
+  @Input() company: string;
+  public ticketsList: Ticket[];
 
-  constructor() { }
+  constructor(
+    private db: Firestore,
+    private localStorage: Storage
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.ticketsList = [];
+
+    Plant.getPlantList(this.db, this.company).then(plants => {
+      plants.forEach(plant => {
+        Ticket.getTickets(this.db, this.company, plant.ref.id, where('voidRequest', '==', true)).then(tickets => {
+          this.ticketsList.push(...tickets);
+        }); 
+      });
+    });
+  }
 
 }
