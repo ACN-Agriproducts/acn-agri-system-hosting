df387649764da9c73710d65e0a8786c0e550945e
rubpu <rubpuga@gmail.com>
2022-08-12 10:40:44 -0500

    Removed ununsed imports from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Storage } from '@angular/fire/storage';
-import { environment } from '@enviroment/environment';
-import { Auth } from '@angular/fire/auth';
-import { SessionInfoService } from '@core/services/session-info/session-info.service';
+
 // import { jsPDF } from "jspdf";
 
 // import html2canvas from 'html2canvas';
