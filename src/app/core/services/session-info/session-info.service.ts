9c00581312c9e806129e2359ab32b5fcd0bcc9fd
rubpu <rubpuga@gmail.com>
2022-08-12 09:59:22 -0500

    Added session-info functions

--- src/app/core/services/session-info/session-info.service.ts
+++ src/app/core/services/session-info/session-info.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { where } from '@angular/fire/firestore';
 import { Storage } from '@ionic/storage';
 
 interface User {
@@ -9,6 +10,9 @@ interface User {
   worksAt: string[],
   currentPermissions: any
 }
+
+declare type keyOpts = 'currentCompany' | 'currentPlant' | 'user'
+
 
 @Injectable({
   providedIn: 'root'
@@ -21,4 +25,42 @@ export class SessionInfoService {
   constructor(
     private localStorage: Storage
   ) { }
+
+  public load(): Promise<void> {
+    const promises = [];
+
+    promises.push(this.localStorage.get('currentCompany').then(val => {
+      this.company = val;
+    }));
+
+    promises.push(this.localStorage.get('currentPlant').then(val => {
+      this.plant = val;
+    }));
+
+    promises.push(this.localStorage.get('user').then(val => {
+      this.user = val;
+    }));
+
+    return Promise.all(promises).then(() => {});
+  }
+
+  public set(key: keyOpts, data: any): Promise<void> {
+    return this.localStorage.set(key, data);
+  }
+
+  public getCompany(): string {
+    return this.company;
+  }
+
+  public getPlant(): string {
+    return this.plant;
+  }
+
+  public getUser(): User {
+    return this.user;
+  }
+
+  public getPermissions(): any {
+    return this.user?.currentPermissions;
+  }
 }
