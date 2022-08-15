c6012346e25d6a09b52a83b09d98dcc43df557d7
rubpu <rubpuga@gmail.com>
2022-08-15 11:44:04 -0500

    Added submit button

--- src/app/shared/components/fix-ticket-storage/fix-ticket-storage.component.ts
+++ src/app/shared/components/fix-ticket-storage/fix-ticket-storage.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
+import { collection, doc, Firestore, runTransaction, serverTimestamp } from '@angular/fire/firestore';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
 import { Inventory, Plant } from '@shared/classes/plant';
 import { Ticket } from '@shared/classes/ticket';
 
@@ -14,7 +16,9 @@ export class FixTicketStorageComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<FixTicketStorageComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Ticket
+    @Inject(MAT_DIALOG_DATA) public data: Ticket,
+    private db: Firestore,
+    private session: SessionInfo
   ) { }
 
   ngOnInit() {
@@ -25,18 +29,50 @@ export class FixTicketStorageComponent implements OnInit {
     });
   }
 
-  getUpdateDoc() {
+  getUpdateDoc(plant: Plant) {
     const updateDoc = [];
+    const changes = [];
+    const storageLog: any = {
+      before: plant.getRawInventory(),
+      updatedBy: this.session.getUser().uid,
+      updatedOn: serverTimestamp(),
+      change: [],
+      updatedType: 'Ticket storage fix',
+    }
 
-    this.plant.inventory.forEach((inv, index) => {
+    plant.inventory.forEach((inv, index) => {
       updateDoc.push(inv.getRawData());
       if(inv.name == this.chosenInv.name) {
         updateDoc[index].current += this.data.dryWeight * (this.data.in? 1 : -1)
+
+        changes.push({
+          type: `Ticket storage fix`,
+          tank: inv.name,
+          amount: this.data.dryWeight * (this.data.in? 1 : -1)
+        })
       }
-    })
+    });
+
+    storageLog.after = updateDoc;
+    storageLog.change = changes;
 
     return {
-      inventory: updateDoc
+      inventory: updateDoc,
+      log: storageLog
     }
+  }
+
+  submitChange() {
+    runTransaction(this.db, transaction => {
+      return transaction.get(this.plant.ref.withConverter(Plant.converter)).then(async plant => {
+        const {inventory, log} = this.getUpdateDoc(plant.data());
+        const logRef = doc(collection(plant.ref, 'storageLogs'));
+        transaction.set(logRef, log);
+        transaction.update(plant.ref, {
+          inventory,
+          lastStorageUpdate: logRef
+        });
+      });
+    });
   }
 }
