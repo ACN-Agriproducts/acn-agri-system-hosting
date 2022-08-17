f30fd2f3644cf24111b7a2bc0084fe951449f137
Jeddie <eduardoreyna0625@hotmail.com>
2022-08-17 16:53:31 -0500

    Changed method name

--- src/app/pages/warehouse-receipts/components/warehouse-receipt-group-card/warehouse-receipt-group-card.component.ts
+++ src/app/pages/warehouse-receipts/components/warehouse-receipt-group-card/warehouse-receipt-group-card.component.ts
@@ -125,8 +125,8 @@ export class WarehouseReceiptGroupCardComponent implements OnInit {
     }
   }
 
-  public onClick() {
-    console.log("Icon click working");
+  public paidWarehouseReceipt() {
+    console.log("Warehouse Receipt has been paid");
   }
 }
 
