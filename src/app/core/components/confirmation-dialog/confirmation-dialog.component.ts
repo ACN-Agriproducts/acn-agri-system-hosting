5202bfa26dc2d23eb524b512a382a38707c1741a
Jeddie <eduardoreyna0625@hotmail.com>
2022-09-13 12:35:48 -0500

    Removed unnecessary line

--- src/app/core/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/core/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -9,8 +9,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class ConfirmationDialogComponent implements OnInit {
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: string,
-    public dialogRef: MatDialogRef<ConfirmationDialogComponent>
+    @Inject(MAT_DIALOG_DATA) public data: string
   ) { }
 
   ngOnInit() {}
