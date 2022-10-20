431db20b924e9691f971fc296034a09a148b1695
Jeddie <eduardoreyna0625@hotmail.com>
2022-10-20 14:02:15 -0500

    Update contract with new field info

--- src/app/core/directive/positive/positive.directive.ts
+++ src/app/core/directive/positive/positive.directive.ts
@@ -17,7 +17,6 @@ export class PositiveDirective {
 
 function validateField(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    // if (control.value == null) return null;
     return control.value <= 0 && control.value != null ? { positive: true } : null;
   }
 }
