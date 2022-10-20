653a80b4dc10611b9b17b5bb32ab730b85472da3
rubpu <rubpuga@gmail.com>
2022-10-20 15:31:28 -0500

    New table dialog

---
+++ src/app/pages/prices/new-table-dialog/new-table-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-new-table-dialog',
+  templateUrl: './new-table-dialog.component.html',
+  styleUrls: ['./new-table-dialog.component.scss'],
+})
+export class NewTableDialogComponent implements OnInit {
+  public returnData: {
+    name: string;
+    baseTable: string;
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<NewTableDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public names: string[]
+  ) { }
+
+  ngOnInit() {
+    this.returnData = {
+      name: "",
+      baseTable: ""
+    }
+  }
+
+  accept() {
+    this.dialogRef.close(this.returnData);
+  }
+}
