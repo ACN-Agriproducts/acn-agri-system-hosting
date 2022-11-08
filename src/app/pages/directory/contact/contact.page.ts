f1d2cf16a18fc8be988bd074519f2901eadbda52
Jeddie <eduardoreyna0625@hotmail.com>
2022-11-08 10:05:56 -0600

    Added pagination, fix pagination delay

--- src/app/pages/directory/contact/contact.page.ts
+++ src/app/pages/directory/contact/contact.page.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Firestore, limit, orderBy, query, where } from '@angular/fire/firestore';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CollectionReference, Firestore, limit, orderBy, Query, query, where } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { SessionInfo } from '@core/services/session-info/session-info.service';
 import { SnackbarService } from '@core/services/snackbar/snackbar.service';
-import { NavController } from '@ionic/angular';
+import { IonInfiniteScroll, NavController } from '@ionic/angular';
 import { Contact } from '@shared/classes/contact';
 import { Contract } from '@shared/classes/contract';
+import { FirebaseDocInterface, Pagination } from '@shared/classes/FirebaseDocInterface';
 import { Ticket } from '@shared/classes/ticket';
 
 @Component({
@@ -14,17 +15,26 @@ import { Ticket } from '@shared/classes/ticket';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   public contact: Contact;
   public contactType: string;
   public contractType: string = 'purchase';
   public currentCompany: string;
   public currentPlant: string;
-  public docList: (Contract | Ticket)[] = [];
+  // public docList: (Contract | Ticket)[] = [];
   public id: string;
   public isToggled: boolean = false;
-  public purchaseContracts: Contract[] = [];
+  // public purchaseContracts: Contract[] = [];
   public ready: boolean = false;
-  public salesContracts: Contract[] = [];
+  // public salesContracts: Contract[] = [];
+  public primaryDocPagination: Pagination<FirebaseDocInterface>;
+  public secondaryDocPagination: Pagination<FirebaseDocInterface>;
+  public docPagination: Pagination<FirebaseDocInterface>;
+  public docCount: number = 0;
+
+  private docLimit: number = 20;
+  private docStep: number = 20;
 
   constructor(
     private db: Firestore,
@@ -61,21 +71,59 @@ export class ContactPage implements OnInit {
   }
 
   public async getTickets(): Promise<void> {
-    const query = [where("truckerId", "==", this.id), orderBy('dateOut')];
-    this.docList = await Ticket.getTickets(this.db, this.currentCompany, this.currentPlant, ...query);
+    const queryList = [
+      where("truckerId", "==", this.id),
+      orderBy('dateOut'),
+      limit(this.docLimit)
+    ];
+    // this.docList = await Ticket.getTickets(this.db, this.currentCompany, this.currentPlant, ...queryList);
+
+    const colQuery = query(
+      Ticket.getCollectionReference(this.db, this.currentCompany, this.currentPlant),
+      ...queryList
+    );
+
+    this.primaryDocPagination = this.setPagination(this.primaryDocPagination, colQuery);
+    this.docPagination = this.primaryDocPagination;
   }
 
   public async getContracts(): Promise<void> {
-    const query = [where("clientName", "==", this.contact.name), orderBy('date')];
-    [this.purchaseContracts, this.salesContracts] = await Contract.getContracts(this.db, this.currentCompany, ...query);
-    this.docList = this.purchaseContracts;
+    const queryList = [
+      where("clientName", "==", this.contact.name),
+      orderBy('date'),
+      limit(this.docLimit)
+    ];
 
-    if (this.docList.length === 0) {
-      this.docList = this.salesContracts;
-      this.contractType = 'sales';
-    }
+    // [this.purchaseContracts, this.salesContracts] = await Contract.getContracts(this.db, this.currentCompany, ...query);
+    // this.docList = this.purchaseContracts;
 
-    console.log(this.purchaseContracts, this.salesContracts);
+    // if (this.docList.length === 0) {
+    //   this.docList = this.salesContracts;
+    //   this.contractType = 'sales';
+    // }
+
+    const primaryColQuery = query(
+      Contract.getCollectionReference(this.db, this.currentCompany, true),
+      ...queryList
+    );
+
+    const secondaryColQuery = query(
+      Contract.getCollectionReference(this.db, this.currentCompany, false),
+      ...queryList
+    );
+
+    this.primaryDocPagination = this.setPagination(this.primaryDocPagination, primaryColQuery);
+    this.secondaryDocPagination = this.setPagination(this.secondaryDocPagination, secondaryColQuery);
+
+    this.docPagination = this.primaryDocPagination;
+  }
+
+  public setPagination(pagination: Pagination<FirebaseDocInterface>, colQuery: Query<FirebaseDocInterface>): Pagination<FirebaseDocInterface> {
+    if (pagination) pagination.end();
+    pagination = new Pagination<FirebaseDocInterface>(colQuery, this.docStep);
+    console.log(pagination.list.length)
+
+    return pagination;
   }
 
   public openContract(refId: string): void {
@@ -84,7 +132,19 @@ export class ContactPage implements OnInit {
 
   public changeContracts = (event: any): void => {
     this.contractType = event.detail.value;
-    this.docList = this.contractType === 'purchase' ? this.purchaseContracts : this.salesContracts;
+    // this.docList = this.contractType === 'purchase' ? this.purchaseContracts : this.salesContracts;
+
+    this.docPagination = this.contractType === 'purchase' ? this.primaryDocPagination : this.secondaryDocPagination;
+  }
+
+  public async infiniteContracts(event: any) {
+    this.docPagination.getNext(snapshot => {
+      event.target.complete();
+
+      if(snapshot.docs.length < this.docStep) {
+        this.infiniteScroll.disabled = true;
+      }
+    });
   }
 
   public edit() {
