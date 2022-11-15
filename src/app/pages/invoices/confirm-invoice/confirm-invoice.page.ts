d5feaa06c76dc2515e7e6318e050a0649a136fe5
rubpu <rubpuga@gmail.com>
2022-11-15 08:52:50 -0600

    Addedprintable invocie to export invocie page

--- src/app/pages/invoices/confirm-invoice/confirm-invoice.page.ts
+++ src/app/pages/invoices/confirm-invoice/confirm-invoice.page.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 import { filter, map } from 'rxjs';
 import { Ticket } from '@shared/classes/ticket';
+import { Company } from '@shared/classes/company';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { Firestore } from '@angular/fire/firestore';
+import { Interface } from 'readline';
+import { contactInfo, item } from '@shared/classes/invoice';
 
 @Component({
   selector: 'app-confirm-invoice',
@@ -10,7 +15,9 @@ import { Ticket } from '@shared/classes/ticket';
   styleUrls: ['./confirm-invoice.page.scss'],
 })
 export class ConfirmInvoicePage implements OnInit {
+  public companyDoc: Company;
   public selectedTickets: Set<Ticket>;
+  public invoice: invoiceInterface
   public groups: {
     [product: string]: {
       [client: string]: {
@@ -22,10 +29,16 @@ export class ConfirmInvoicePage implements OnInit {
   }
 
   constructor(
-    private router: Router
+    private router: Router,
+    private session: SessionInfo,
+    private db: Firestore
   ) { }
 
   ngOnInit() {
+    Company.getCompany(this.db, this.session.getCompany()).then(company => {
+      this.companyDoc = company;
+      this.invoice.id = company.nextInvoice;
+    });
     this.selectedTickets = this.router.getCurrentNavigation().extras.state as Set<Ticket>;
     this.groups = {};
 
@@ -46,6 +59,40 @@ export class ConfirmInvoicePage implements OnInit {
       object.tickets.push(ticket);
       object.totalWeight += ticket.gross - ticket.tare;
     });
+
+    this.invoice = {
+      buyer: {
+        city: null,
+        country: null,
+        name: null,
+        phone: null,
+        state: null,
+        street: null,
+        zip: null
+      },
+      date: new Date(),
+      id: 0,
+      items: [],
+      seller: {
+        city: null,
+        country: null,
+        name: null,
+        phone: null,
+        state: null,
+        street: null,
+        zip: null
+      },
+      total: 0
+    }
   }
 
+}
+
+interface invoiceInterface {
+  buyer: contactInfo;
+  date: Date;
+  id: number;
+  items: item[];
+  seller: contactInfo;
+  total: number;
 }
