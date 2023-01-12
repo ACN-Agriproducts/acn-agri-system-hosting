926d1ae48528b1dfaf6bf2042ad998f49186bca0
rubpu <rubpuga@gmail.com>
2023-01-12 15:36:12 -0600

    refactored inventory page to be more declarative

--- src/app/pages/inventory/inventory.page.ts
+++ src/app/pages/inventory/inventory.page.ts
@@ -1,7 +1,6 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { ModalController, NavController, PopoverController } from '@ionic/angular';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { firstValueFrom, Observable } from 'rxjs';
 import { NewStorageModalComponent } from './components/new-storage-modal/new-storage-modal.component';
 import { StoragePopoverComponent } from './components/storage-popover/storage-popover.component';
 import { Plant } from '@shared/classes/plant';
@@ -16,59 +15,34 @@ import { MatMenuTrigger } from '@angular/material/menu';
   templateUrl: './inventory.page.html',
   styleUrls: ['./inventory.page.scss'],
 })
-export class InventoryPage implements OnInit, OnDestroy{
-
-  public currentCompany: string;
-  public currentPlantName: string;
-  public plantList: Plant[];
-  public productList: Product[];
-  public currentSubs: Subscription[] = [];
-  public dataUser: any;
-  public permissions: any;
+export class InventoryPage implements OnInit {
   public showArchivedInv: boolean = false;
+
+  public plant$: Observable<Plant>;
+  public products$: Observable<Product[]>;
 
   @ViewChild('InvMenuTrigger') InvMenuTrigger: HTMLElement;
   @ViewChild(MatMenuTrigger) contextMenu: MatMenuTrigger;
 
   constructor(
     private db: Firestore,
-    private navController: NavController,
     private modalController: ModalController,
     private popoverController: PopoverController,
     private fns: Functions,
-    private session: SessionInfo
+    public session: SessionInfo
   ) {}
 
   ngOnInit() {
-    this.dataUser = this.session.getUser();
-    this.permissions = this.session.getPermissions();
-    this.currentCompany = this.session.getCompany();
-    this.currentPlantName = this.session.getPlant();
-
-    var tempSub;
-    tempSub = Plant.getCollectionSnapshot(this.db, this.currentCompany).subscribe(val => {
-      this.plantList = val;
-    })
-    this.currentSubs.push(tempSub);
-
-    tempSub = Product.getCollectionSnapshot(this.db, this.currentCompany).subscribe(val => {
-      this.productList = val;
-    })
-    this.currentSubs.push(tempSub);
+    this.products$ = Product.getCollectionSnapshot(this.db, this.session.getCompany());
+    this.getPlantSnapshot();
   }
 
-  ngOnDestroy() {
-    for(const sub of this.currentSubs) {
-      sub.unsubscribe();
-    }
+  getPlantSnapshot() {
+    this.plant$ = Plant.getPlantSnapshot(this.db, this.session.getCompany(), this.session.getPlant());
   }
 
   plantChange(newPlant: Plant) {
-    this.currentPlantName = newPlant.ref.id;
-  }
-
-  public nav(path:string): void {
-    this.navController.navigateForward(path);
+    this.getPlantSnapshot();
   }
 
   public async inventoryMenu(ev: any, storageId: number): Promise<void> {
@@ -76,10 +50,10 @@ export class InventoryPage implements OnInit, OnDestroy{
       component: StoragePopoverComponent,
       event: ev,
       componentProps: {
-        plantRef: this.getCurrentPlant().ref,
+        plantRef: (await firstValueFrom(this.plant$)).ref,
         storageId: storageId,
-        tankList: this.getCurrentPlant().inventory,
-        productList: this.productList
+        tankList: (await firstValueFrom(this.plant$)).inventory,
+        productList: await firstValueFrom(this.products$)
       }
     });
 
@@ -90,24 +64,16 @@ export class InventoryPage implements OnInit, OnDestroy{
     const modal = await this.modalController.create({
       component: NewStorageModalComponent,
       componentProps:{
-        plantRef: Plant.getDocReference(this.db, this.currentCompany, this.currentPlantName),
-        productList: this.productList,
+        plantRef: Plant.getDocReference(this.db, this.session.getCompany(), this.session.getPlant()),
+        productList: await firstValueFrom(this.products$),
       }
     });
 
     return await modal.present();
   }
 
-  // public hasReadPermission = (): Boolean => {
-  //   return this.permissions?.developer || this.permissions?.admin || this.permissions?.inventory?.warehouseReceiptRead;
-  // }
-
-  public getCurrentPlant(): Plant {
-    return this.plantList.find(p => p.ref.id == this.currentPlantName);
-  }
-
   public updateProductInv(): void {
-    httpsCallable(this.fns, 'helperFunctions-calculateProductValues')({company: this.currentCompany});
+    httpsCallable(this.fns, 'helperFunctions-calculateProductValues')({company: this.session.getCompany()});
   }
 
   public onContextMenu(event: MouseEvent) {
