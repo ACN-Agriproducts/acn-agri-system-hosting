4b95a465be80abce78b0047819a17f3712297a87
Pugster01 <rubpuga@gmail.com>
2023-01-16 14:55:52 -0600

    Added week range select

--- src/app/pages/inventory/storage-logs/storage-logs.page.ts
+++ src/app/pages/inventory/storage-logs/storage-logs.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { DateAdapter } from '@angular/material/core';
+import { DateRange, MatDateRangeSelectionStrategy, MAT_DATE_RANGE_SELECTION_STRATEGY } from '@angular/material/datepicker';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+
+@Injectable()
+export class WeekRangeSelectionStrategy<D> implements MatDateRangeSelectionStrategy<D> {
+  constructor(private _dateAdapter: DateAdapter<D>) {}
+
+  selectionFinished(date: D): DateRange<D> {
+    return this._createWeekRange(date);
+  }
+
+  createPreview(activeDate: D): DateRange<D> {
+    return this._createWeekRange(activeDate);
+  }
+
+  private _createWeekRange(date: D | null): DateRange<D> {
+    if(date) {
+      const day = this._dateAdapter.getDayOfWeek(date);
+
+      const start = this._dateAdapter.addCalendarDays(date, -day);
+      const end = this._dateAdapter.addCalendarDays(date, 6-day);
+      return new DateRange<D>(start, end);
+    }
+
+    return new DateRange<D>(null, null);
+  }
+}
 
 @Component({
   selector: 'app-storage-logs',
   templateUrl: './storage-logs.page.html',
   styleUrls: ['./storage-logs.page.scss'],
+  providers: [
+    {
+      provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
+      useClass: WeekRangeSelectionStrategy
+    }
+  ]
 })
 export class StorageLogsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private session: SessionInfo,
+    private db: Firestore
+  ) { }
 
   ngOnInit() {
+
   }
 
 }
