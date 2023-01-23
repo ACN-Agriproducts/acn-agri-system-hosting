666b90519547c1acc5ca94d957308764c870143b
rubpu <rubpuga@gmail.com>
2023-01-23 15:35:02 -0600

    Refactor contracts page to add new display type

--- src/app/pages/contracts/contracts.page.ts
+++ src/app/pages/contracts/contracts.page.ts
@@ -1,18 +1,15 @@
 import { IonInfiniteScroll, ModalController, NavController, PopoverController } from '@ionic/angular';
 import { ContractModalComponent } from './components/contract-modal/contract-modal.component';
-import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { UntypedFormControl } from '@angular/forms';
-import { OptionFilterComponent } from './components/option-filter/option-filter.component';
 import { DataContractService } from './../../core/data/data-contract.service';
 import { OptionsContractComponent } from './components/options-contract/options-contract.component';
-import { Storage } from '@ionic/storage';
 import { ContractModalOptionsComponent } from './components/contract-modal-options/contract-modal-options.component';
-import { Subscription } from 'rxjs';
-import { Firestore, limit, orderBy, query, where } from '@angular/fire/firestore';
+import { CollectionReference, Firestore, getDocs, limit, orderBy, query, QuerySnapshot, where } from '@angular/fire/firestore';
 import { Contract } from '@shared/classes/contract';
-import { Pagination } from '@shared/classes/FirebaseDocInterface';
-import { Mass } from '@shared/classes/mass';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { startAfter } from 'firebase/firestore';
 
 
 @Component({
@@ -20,25 +17,32 @@ import { Mass } from '@shared/classes/mass';
   templateUrl: './contracts.page.html',
   styleUrls: ['./contracts.page.scss'],
 })
-export class ContractsPage implements OnInit, AfterViewInit, OnDestroy {
+export class ContractsPage implements AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  @ViewChild("infiniteScroll") infiniteScroll: IonInfiniteScroll;
+  @ViewChild("cards") cards: TemplateRef<any>;
+  @ViewChild("table") table: TemplateRef<any>;
 
   public searchIinput = new UntypedFormControl('');
   public ready: boolean = false;
   public sortField: string = "date";
   public assending: boolean = false;
-  public dataList: Contract[][] = [];
-  public dataListAux: any;
   public listFilter: any = [];
   public activeFilter: boolean;
-  public currentCompany: string;
-  public contractType: string = "purchaseContracts";
   public orderStatus: string[] = ["active", "closed", "pending", "canceled"];
-  public currentSub: Subscription[] = [];
-  public contractPagination: Pagination<Contract>;
 
-  private contractLimit = 20;
+  public tabData: {
+    label: string;
+    type: TemplateRef<any>;
+    isInfiniteScrollDisabled: boolean;
+    data: {
+      ref: CollectionReference<Contract>;
+      title?: string;
+      contracts: Promise<QuerySnapshot<Contract>>[];
+    }[]
+  }[];
+  public tabIndex: number = 0;
+
   private contractStep = 20;
 
   constructor(
@@ -47,51 +51,102 @@ export class ContractsPage implements OnInit, AfterViewInit, OnDestroy {
     private cd: ChangeDetectorRef,
     private dataService: DataContractService,
     private db: Firestore,
-    private localStorage: Storage,
+    private session: SessionInfo,
     private navController: NavController
   ) { }
 
-  ngOnInit() {
-    this.localStorage.get('currentCompany').then(val => {
-      this.currentCompany = val;
-      this.getContracts();
-    })
+  ngAfterViewInit() {
+    this.tabData = [{
+      label: "Purchase Contracts",
+      type: this.table,
+      isInfiniteScrollDisabled: false,
+      data: [
+        {
+          ref: Contract.getCollectionReference(this.db, this.session.getCompany(), true), 
+          contracts: [],
+        }
+      ]
+    },{
+      label: "Sales Contracts",
+      type: this.table,
+      isInfiniteScrollDisabled: false,
+      data: [
+        {
+          ref: Contract.getCollectionReference(this.db, this.session.getCompany(), false), 
+          contracts: [],
+        }
+      ]
+    },{
+      label: "Analytics",
+      type: this.cards,
+      isInfiniteScrollDisabled: false,
+      data: [
+        {
+          ref: Contract.getCollectionReference(this.db, this.session.getCompany(), true), 
+          title: "Purchase Contracts", 
+          contracts: [],
+        },
+        {
+          ref: Contract.getCollectionReference(this.db, this.session.getCompany(), false), 
+          title: "Purchase Contracts", 
+          contracts: [],
+        }
+      ]
+    }];
+
+    this.getContracts();
   }
 
-  ngAfterViewInit() {
-  }
-  public segmentChanged = (event) => {
+  public segmentChanged(event) {
     this.orderStatus = event.detail.value.split(',');
     this.getContracts();
   };
 
-  public changedContractType(event){
-    this.contractType = event.detail.value;
+  public changedContractType(event) {
+    this.tabIndex = event.detail.value;
     this.getContracts();
   };
 
   public async getContracts() {
-    for(const sub of this.currentSub){
-      sub.unsubscribe();
-    };
+    const currentTabData = this.tabData[this.tabIndex];
+    // this.infiniteScroll.disabled = currentTabData.isInfiniteScrollDisabled;
+    
+    currentTabData.data.forEach(data => {
+      if(data.contracts.length == 0) {
+        const dbQuery = query(
+          data.ref,
+          where("status", "in", this.orderStatus),
+          orderBy(this.sortField, this.assending? 'asc': 'desc'),
+          limit(this.contractStep)
+        );
 
-    this.currentSub = [];
-    this.contractLimit = this.contractStep;
-    this.infiniteScroll.disabled = false;
-    const ColQuery = query(Contract.getCollectionReference(this.db, this.currentCompany, this.contractType == 'purchaseContracts'),
-                    where("status", "in", this.orderStatus),
-                    orderBy(this.sortField, this.assending? 'asc': 'desc'),
-                    limit(this.contractLimit));
-
-    if(this.contractPagination) this.contractPagination.end();
-    this.contractPagination = new Pagination<Contract>(ColQuery, 20);
+        data.contracts.push(getDocs(dbQuery));
+      }
+    });
   };
 
   public async infiniteContracts(event) {
-    this.contractPagination.getNext(snapshot => {
-      event.target.complete();
-      this.infiniteScroll.disabled = snapshot.docs.length < this.contractStep;
-    });
+    const currentTabData = this.tabData[this.tabIndex];
+    const promises = [];
+
+    for(let data of currentTabData.data) {
+      const dbQuery = query(
+        data.ref,
+        where("status", "in", this.orderStatus),
+        orderBy(this.sortField, this.assending? 'asc': 'desc'),
+        startAfter((await data.contracts[-1]).docs[-1]),
+        limit(this.contractStep)
+      );
+      
+      const promise = getDocs(dbQuery);
+
+      promises.push(promise);
+      data.contracts.push(promise);
+    }
+
+    const snapshot = await Promise.all(promises);
+    event.target.complete();
+    this.infiniteScroll.disabled = snapshot[0].docs.length < this.contractStep;
   }
 
   public openModal = async () => {
@@ -102,38 +157,38 @@ export class ContractsPage implements OnInit, AfterViewInit, OnDestroy {
     return await modal.present();
   }
 
-  public openOptionsFilter = async (event, objet?: string, typeObjet?: string) => {
-    const popover = await this.popoverController.create({
-      component: OptionFilterComponent,
-      event,
-      cssClass: 'option-filter',
-      // showBackdrop: false,
-      componentProps: {
-        listDataAlter: this.dataList,
-        listData: this.dataService.dataContract, objet,
-        listFilter: this.listFilter,
-        typeObjet
-      }
-    });
-    await popover.present();
-    await popover.onDidDismiss().then(result => {
-      const data = result.data;
-      if (data) {
-        this.dataList = result.data.list;
-        this.dataListAux = this.dataList;
-        this.listFilter = result.data.filter;
-        this.activeFilter = true;
-        // if (data.filterDate) {
+  // public openOptionsFilter = async (event, objet?: string, typeObjet?: string) => {
+  //   const popover = await this.popoverController.create({
+  //     component: OptionFilterComponent,
+  //     event,
+  //     cssClass: 'option-filter',
+  //     // showBackdrop: false,
+  //     componentProps: {
+  //       listDataAlter: this.dataList,
+  //       listData: this.dataService.dataContract, objet,
+  //       listFilter: this.listFilter,
+  //       typeObjet
+  //     }
+  //   });
+  //   await popover.present();
+  //   await popover.onDidDismiss().then(result => {
+  //     const data = result.data;
+  //     if (data) {
+  //       this.dataList = result.data.list;
+  //       this.dataListAux = this.dataList;
+  //       this.listFilter = result.data.filter;
+  //       this.activeFilter = true;
+  //       // if (data.filterDate) {
 
-        //   this.dataListAux = this.filterDateRange(result.data.list, data.filterDate);
-        // }
-        this.cd.markForCheck();
-        // } else {
-        //   this.dataListAux = this.filterDateRange(data.dataListAux, data.dataDate);
-        // }
-      }
-    });
-  }
+  //       //   this.dataListAux = this.filterDateRange(result.data.list, data.filterDate);
+  //       // }
+  //       this.cd.markForCheck();
+  //       // } else {
+  //       //   this.dataListAux = this.filterDateRange(data.dataListAux, data.dataDate);
+  //       // }
+  //     }
+  //   });
+  // }
 
   public openOptions = async (event) => {
     const popover = await this.popoverController.create({
@@ -159,19 +214,18 @@ export class ContractsPage implements OnInit, AfterViewInit, OnDestroy {
     this.navController.navigateForward('dashboard/contracts/new-contract')
   }
 
-  public openContractOptions= async (event: any, id: string, contract: any) => {
+  public openContractOptions= async (event: any, contract: Contract) => {
     event.preventDefault();
-    let user = await this.localStorage.get('user')
     const popover = await this.popoverController.create({
       component: ContractModalOptionsComponent,
       event,
       dismissOnSelect: true,
       componentProps: {
-        contractId: id,
-        isPurchase: this.contractType == 'purchaseContracts',
+        contractId: contract.ref.id,
+        isPurchase: contract.ref.parent.id == 'purchaseContracts',
         status: contract.status,
-        currentCompany: this.currentCompany,
-        userPermissions: user.permissions,
+        currentCompany: this.session.getCompany(),
+        userPermissions: this.session.getPermissions(),
         contract: contract
       }
     })
@@ -192,9 +246,5 @@ export class ContractsPage implements OnInit, AfterViewInit, OnDestroy {
     return `${weight.getMassInUnit('lbs').toFixed(3)} lbs
     ${weight.getBushelWeight(contract.productInfo).toFixed(3)} bu
     ${weight.getMassInUnit('mTon').toFixed(3)} mTon`
-  }
-
-  ngOnDestroy(): void {
-      this.contractPagination.end();
   }
 }
