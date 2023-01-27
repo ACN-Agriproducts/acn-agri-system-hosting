bb5f50ee1691b2497347129c999092fa8278f7c7
rubpu <rubpuga@gmail.com>
2023-01-27 14:02:45 -0600

    Printable invoice two header set

--- src/app/shared/printable/printable-invoice/invoice-two/invoice-two.component.ts
+++ src/app/shared/printable/printable-invoice/invoice-two/invoice-two.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { contactInfo, Invoice, item } from '@shared/classes/invoice';
 
 @Component({
   selector: 'app-invoice-two',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceTwoComponent implements OnInit {
 
+  @Input() invoice: Invoice;
+  @Input() seller: contactInfo;
+  @Input() buyer: contactInfo;
+  @Input() id: number | string;
+  @Input() date: Date;
+  @Input() items: item[];
+  @Input() total: number;
+
   constructor() { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+  }
 }
