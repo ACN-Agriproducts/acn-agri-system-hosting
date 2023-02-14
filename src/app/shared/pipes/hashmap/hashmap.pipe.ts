6abf1b28f1ac7ce8b1369368b1a303f7f712410a
Jeddie <eduardoreyna0625@hotmail.com>
2023-02-14 09:54:51 -0600

    Removed unnecessary comments

--- src/app/shared/pipes/hashmap/hashmap.pipe.ts
+++ src/app/shared/pipes/hashmap/hashmap.pipe.ts
@@ -4,11 +4,7 @@ import { Pipe, PipeTransform, TemplateRef } from '@angular/core';
   name: 'hashmap'
 })
 export class HashMapPipe implements PipeTransform {
-
-  // passing in parent component and referencing with [] - X
-  // evals - X
-  // passing in array of templateRefs and finding by name - X
-  // 
+  
   transform<K, U>(templateMap: Map<K, U>, key: K): U {
     return templateMap?.get(key) ?? null;
   }
