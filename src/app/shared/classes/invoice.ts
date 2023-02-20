54d86878f8f0475277040e8825a72c10724f26be
Pugster01 <rubpuga@gmail.com>
2023-02-20 10:30:33 -0600

    fix field in which proof of payment is saved

--- src/app/shared/classes/invoice.ts
+++ src/app/shared/classes/invoice.ts
@@ -8,6 +8,7 @@ export class Invoice extends FirebaseDocInterface {
     public items: item[];
     public needsAttention: boolean;
     public pdfReference: string;
+    public proofLinks: string[];
     public seller: contactInfo;
     public status: string;
     public total: number;
@@ -23,6 +24,7 @@ export class Invoice extends FirebaseDocInterface {
         this.items = [];
         this.needsAttention = data.needsAttention;
         this.pdfReference = data.pdfReference;
+        this.proofLinks = data.proofLinks;
         this.seller = new contactInfo(data.seller);
         this.status = data.status;
         this.total = data.total;
