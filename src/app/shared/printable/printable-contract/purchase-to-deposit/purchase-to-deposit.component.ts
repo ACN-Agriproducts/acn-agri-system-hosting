3c523fcdd957f7dc6e2264e48a3f6f92c93b2326
Jeddie <eduardoreyna0625@hotmail.com>
2023-03-02 16:20:53 -0600

    Finished adjustments to third-party-warehouse printable component

--- src/app/shared/printable/printable-contract/purchase-to-deposit/purchase-to-deposit.component.ts
+++ src/app/shared/printable/printable-contract/purchase-to-deposit/purchase-to-deposit.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'contract-purchase-to-deposit',
   templateUrl: './purchase-to-deposit.component.html',
-  styleUrls: ['../printable-contract-styles.scss'],
+  styleUrls: [
+    '../printable-contract-styles.scss',
+    './purchase-to-deposit.component.scss'
+  ],
 })
 export class PurchaseToDepositComponent implements OnInit {
 
--- src/app/shared/printable/printable-contract/third-party-warehouse/third-party-warehouse.component.ts
+++ src/app/shared/printable/printable-contract/third-party-warehouse/third-party-warehouse.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'contract-third-party-warehouse',
   templateUrl: './third-party-warehouse.component.html',
-  styleUrls: ['../printable-contract-styles.scss'],
+  styleUrls: [
+    '../printable-contract-styles.scss', 
+    './third-party-warehouse.component.scss'
+  ],
 })
 export class ThirdPartyWarehouseComponent implements OnInit {
 
