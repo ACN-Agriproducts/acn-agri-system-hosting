f0e21db0746fdae7651508d32cc708b2697877db
Jeddie <eduardoreyna0625@hotmail.com>
2023-03-06 15:20:03 -0600

    Finished content for sell-contract printable

--- src/app/shared/printable/printable-contract/sell-contract/sell-contract.component.ts
+++ src/app/shared/printable/printable-contract/sell-contract/sell-contract.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'sell-contract',
   templateUrl: './sell-contract.component.html',
-  styleUrls: ['./sell-contract.component.scss'],
+  styleUrls: [
+    './sell-contract.component.scss',
+    '../printable-contract-styles.scss'
+  ],
 })
 export class SellContractComponent implements OnInit {
 
