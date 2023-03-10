0aedb9381975f0b7fe8bf13af809bce6dccb9549
rubpu <rubpuga@gmail.com>
2023-03-10 07:30:12 -0600

    bagging order form

--- src/app/pages/bagging-orders/set-order/set-order.page.ts
+++ src/app/pages/bagging-orders/set-order/set-order.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { doc, Firestore } from '@angular/fire/firestore';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { BaggingOrder } from '@shared/classes/bagging-order';
+import { InvoiceItem } from '@shared/classes/invoice_item';
+import { Plant } from '@shared/classes/plant';
+import { User } from '@shared/classes/user';
 
 @Component({
   selector: 'app-set-order',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-order.page.scss'],
 })
 export class SetOrderPage implements OnInit {
+  order: BaggingOrder;
+  plant: Plant;
 
-  constructor() { }
+  invoiceItems$: Promise<InvoiceItem[]>;
+  plants$: Promise<Plant[]>
+
+  constructor(
+    private session: SessionInfo,
+    private db: Firestore
+  ) { }
 
   ngOnInit() {
+    this.order = new BaggingOrder();
+    this.order.orderInfo = [{
+      quantity: null,
+      itemRef: null,
+      affectsInventory: null
+    }];
+
+    this.invoiceItems$ = InvoiceItem.getList(this.db, this.session.getCompany());
+    this.plants$ = Plant.getPlantList(this.db, this.session.getCompany());
+  }
+
+  submit() {
+    this.order.date = new Date();
+    this.order.status = "pending";
+    this.order.orderOwner = User.getDocumentReference(this.db, this.session.getUser().uid);
+    this.order.orderOwnerName = this.session.getUser().name;
+    this.order.ref = doc(BaggingOrder.getCollectionReference(this.db, this.session.getCompany(), this.plant.ref.id));
   }
 
 }
