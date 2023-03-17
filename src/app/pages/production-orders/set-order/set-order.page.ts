a9133424d36ebbd87b03b22f4bd5662828410731
Pugster01 <rubpuga@gmail.com>
2023-03-17 10:11:54 -0500

    set order form v1

--- src/app/pages/production-orders/set-order/set-order.page.ts
+++ src/app/pages/production-orders/set-order/set-order.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { doc, Firestore } from '@angular/fire/firestore';
+import { doc, DocumentReference, Firestore } from '@angular/fire/firestore';
 import { SessionInfo } from '@core/services/session-info/session-info.service';
 import { ProductionOrder } from '@shared/classes/production-order';
 import { InvoiceItem } from '@shared/classes/invoice_item';
@@ -24,32 +24,49 @@ export class SetOrderPage implements OnInit {
 
   ngOnInit() {
     this.order = new ProductionOrder();
-    this.order.orderInfo = [{
-      quantity: null,
-      name: null,
-      itemRef: null,
-      affectsInventory: null
-    }];
+    this.order.orderInfo = [];
+    this.addItem();
 
     this.invoiceItems$ = InvoiceItem.getList(this.db, this.session.getCompany());
     this.plants$ = Plant.getPlantList(this.db, this.session.getCompany());
   }
 
-  async submit() {
-    const itemsList = await this.invoiceItems$;
-    const chosenItem = itemsList.find(item => item.ref == this.order.orderInfo[0].itemRef);
-    this.order.orderInfo[0].name = chosenItem.name;
+  addItem(): void {
+    this.order.orderInfo.push({
+      quantity: null,
+      name: null,
+      itemRef: null,
+      affectsInventory: null
+    });
+  }
 
+  async itemSelected(item: orderItem): Promise<void> {
+    const newItem = (await this.invoiceItems$).find(item => item.ref == this.order.orderInfo[0].itemRef);
+    item.name = newItem.name;
+  }
+
+  deleteItem(index: number): void {
+    this.order.orderInfo.splice(index, 1)
+  }
+
+  async submit() {
     this.order.date = new Date();
     this.order.status = "pending";
     this.order.orderOwner = User.getDocumentReference(this.db, this.session.getUser().uid);
     this.order.orderOwnerName = this.session.getUser().name;
     this.order.ref = doc(ProductionOrder.getCollectionReference(this.db, this.session.getCompany()));
-    this.order.id = 1;
+    this.order.id = 1; //TODO
     this.order.fulfilledDate = null;
     this.order.docRefs = [];
     
-    this.order.set();
+    console.log(this.order);
+    //this.order.set();
   }
+}
 
+interface orderItem {
+  quantity: number;
+  name: string;
+  itemRef: DocumentReference;
+  affectsInventory: boolean;
 }
