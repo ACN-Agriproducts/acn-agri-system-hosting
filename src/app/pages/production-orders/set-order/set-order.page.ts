f2e00180959000e795480d2d8ac2b33472d33c6f
Pugster01 <rubpuga@gmail.com>
2023-03-17 14:46:02 -0500

    Production order form add info

--- src/app/pages/production-orders/set-order/set-order.page.ts
+++ src/app/pages/production-orders/set-order/set-order.page.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { doc, DocumentReference, Firestore } from '@angular/fire/firestore';
+import { doc, Firestore, getDocsFromServer, limit, orderBy, query } from '@angular/fire/firestore';
 import { SessionInfo } from '@core/services/session-info/session-info.service';
-import { ProductionOrder } from '@shared/classes/production-order';
+import { orderItem, orderItemInfo, ProductionOrder } from '@shared/classes/production-order';
 import { InvoiceItem } from '@shared/classes/invoice_item';
 import { Plant } from '@shared/classes/plant';
 import { User } from '@shared/classes/user';
+import { Product } from '@shared/classes/product';
+import { Router } from '@angular/router';
+import { SnackbarService } from '@core/services/snackbar/snackbar.service';
 
 @Component({
   selector: 'app-set-order',
@@ -13,13 +16,17 @@ import { User } from '@shared/classes/user';
 })
 export class SetOrderPage implements OnInit {
   order: ProductionOrder;
+  plant: Plant;
 
   invoiceItems$: Promise<InvoiceItem[]>;
+  productsList$: Promise<Product[]>;
   plants$: Promise<Plant[]>
 
   constructor(
     private session: SessionInfo,
-    private db: Firestore
+    private db: Firestore,
+    private router: Router,
+    private snack: SnackbarService
   ) { }
 
   ngOnInit() {
@@ -29,15 +36,20 @@ export class SetOrderPage implements OnInit {
 
     this.invoiceItems$ = InvoiceItem.getList(this.db, this.session.getCompany());
     this.plants$ = Plant.getPlantList(this.db, this.session.getCompany());
+    this.productsList$ = Product.getProductList(this.db, this.session.getCompany());
+  }
+
+  async plantChange(): Promise<void> {
+    this.plant = (await this.plants$).find(p => p.ref == this.order.plant);
   }
 
   addItem(): void {
-    this.order.orderInfo.push({
+    this.order.orderInfo.push(new orderItem({
       quantity: null,
       name: null,
       itemRef: null,
       affectsInventory: null
-    });
+    }));
   }
 
   async itemSelected(item: orderItem): Promise<void> {
@@ -49,6 +61,23 @@ export class SetOrderPage implements OnInit {
     this.order.orderInfo.splice(index, 1)
   }
 
+  affectsInventoryChange(item: orderItem) {
+    if (item.affectsInventory) {
+      item.inventoryInfo = [new orderItemInfo({})]
+    }
+    else {
+      item.inventoryInfo = null;
+    }
+  }
+
+  addItemInfo(item: orderItem) {
+    item.inventoryInfo.push(new orderItemInfo({}));
+  }
+
+  deleteItemInfo(item: orderItem, index: number) {
+    item.inventoryInfo.splice(index, 1);
+  }
+
   async submit() {
     this.order.date = new Date();
     this.order.status = "pending";
@@ -58,15 +87,22 @@ export class SetOrderPage implements OnInit {
     this.order.id = 1; //TODO
     this.order.fulfilledDate = null;
     this.order.docRefs = [];
-    
-    console.log(this.order);
-    //this.order.set();
-  }
-}
 
-interface orderItem {
-  quantity: number;
-  name: string;
-  itemRef: DocumentReference;
-  affectsInventory: boolean;
+    const queryLast = query(
+      ProductionOrder.getCollectionReference(this.db, this.session.getCompany()), 
+      orderBy("id", "desc"), limit(1)
+    );
+    getDocsFromServer(queryLast).then(result => {
+      this.order.id = result.empty ? 1 :
+        result.docs[0].data().id + 1;
+
+      return this.order.set();
+    }).then(() => {
+      this.router.navigate(["dashboard/production-orders"]);
+      this.snack.open("Order successfully updated", "success");
+    }).catch(error => {
+      this.snack.open("Submit unsuccessful", "error");
+      console.error(error);
+    });
+  }
 }
--- src/app/shared/classes/production-order.ts
+++ src/app/shared/classes/production-order.ts
@@ -11,12 +11,7 @@ export class ProductionOrder extends FirebaseDocInterface {
     docRefs: string[];
     orderOwner: DocumentReference;
     orderOwnerName: string;
-    orderInfo: {
-        quantity: number;
-        name: string;
-        itemRef: DocumentReference;
-        affectsInventory: boolean;
-    }[];
+    orderInfo: orderItem[];
     plant: DocumentReference<Plant>;
 
     constructor();
@@ -44,12 +39,17 @@ export class ProductionOrder extends FirebaseDocInterface {
         this.docRefs = data.docRefs;
         this.orderOwner = data.orderOwner;
         this.orderOwnerName = data.orderOwnerName;
-        this.orderInfo = data.orderInfo;
+        this.orderInfo = [];
+
+        data.orderInfo.forEach(element => {
+            this.orderInfo.push(element);
+        });
+        
     }
 
     public static converter = {
         toFirestore(data: ProductionOrder): DocumentData {
-            return {
+            const doc = {
                 date: data.date,
                 fulfilledDate: data.fulfilledDate,
                 id: data.id,
@@ -57,9 +57,25 @@ export class ProductionOrder extends FirebaseDocInterface {
                 docRefs: data.docRefs,
                 orderOwner: data.orderOwner,
                 orderOwnerName: data.orderOwnerName,
-                orderInfo: data.orderInfo,
+                orderInfo: [],
                 plant: data.plant,
             }
+
+            data.orderInfo.forEach((item, index) => {
+                doc.orderInfo.push({
+                    quantity: item.quantity,
+                    name: item.name,
+                    itemRef: item.itemRef,
+                    affectsInventory: item.affectsInventory,
+                    inventoryInfo: [],
+                });
+
+                item.inventoryInfo.forEach(info => {
+                    doc.orderInfo[index].inventoryInfo.info.push({...info});
+                });
+            });
+
+            return doc;
         },
         fromFirestore(snapshot: QueryDocumentSnapshot<any>, options: SnapshotOptions): ProductionOrder {
             return new ProductionOrder(snapshot);
@@ -76,5 +92,37 @@ export class ProductionOrder extends FirebaseDocInterface {
 
     public static getCollectionReference(db: Firestore, company: string): CollectionReference<ProductionOrder> {
         return collection(Company.getCompanyRef(db, company), "productionOrders").withConverter(ProductionOrder.converter);
+    }
+}
+
+export class orderItem {
+    quantity: number;
+    name: string;
+    itemRef: DocumentReference;
+    affectsInventory: boolean;
+    inventoryInfo: orderItemInfo[];
+
+    constructor(data: any) {
+        this.quantity = data.quantity;
+        this.name = data.name;
+        this.itemRef = data.itemRef;
+        this.affectsInventory = data.affectsInventory;
+        this.inventoryInfo = [];
+
+        data.inventoryInfo?.info.forEach(element => {
+            this.inventoryInfo.push(element);
+        });
+    }
+}
+
+export class orderItemInfo {
+    public product: string;
+    public quantity: number;
+    public tank: string;
+
+    constructor(data: any) {
+        this.product = data.product;
+        this.quantity = data.quantity;
+        this.tank = data.tank;
     }
 }
