2dd5401663c84a14a399db4aa35af6c5b39eaae8
rubpu <rubpuga@gmail.com>
2023-03-22 14:02:39 -0500

    contract form primitive template

--- src/app/pages/contracts/components/contract-form/contract-form.component.ts
+++ src/app/pages/contracts/components/contract-form/contract-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { Contract } from '@shared/classes/contract';
+import { ContractSettings } from '@shared/classes/contract-settings';
 
 @Component({
   selector: 'app-contract-form',
@@ -6,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-form.component.scss'],
 })
 export class ContractFormComponent implements OnInit {
+  @Input() contract: Contract;
+  public settings: ContractSettings;
 
-  constructor() { }
+  constructor(
+    private db: Firestore,
+    private session: SessionInfo
+  ) { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    ContractSettings.getDocument(this.db, this.session.getCompany()).then(result => {
+      this.settings = result;
+    });
+  }
 }
