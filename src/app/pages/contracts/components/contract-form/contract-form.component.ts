4d457acedc85f6cffd55178fa4d80aced21bdffc
rubpu <rubpuga@gmail.com>
2023-03-23 08:10:12 -0500

    Added client fields

--- src/app/pages/contracts/components/contract-form/contract-form.component.ts
+++ src/app/pages/contracts/components/contract-form/contract-form.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input, OnInit, QueryList, TemplateRef, ViewChildren } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
+import { MatDialog } from '@angular/material/dialog';
 import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { Company, CompanyContact } from '@shared/classes/company';
+import { Contact } from '@shared/classes/contact';
 import { Contract } from '@shared/classes/contract';
 import { ContractSettings } from '@shared/classes/contract-settings';
 import { TypeTemplateDirective } from '@shared/directives/type-template/type-template.directive';
+import { lastValueFrom } from 'rxjs';
+import { SelectClientComponent } from '../select-client/select-client.component';
 
 @Component({
   selector: 'app-contract-form',
@@ -13,17 +18,70 @@ import { TypeTemplateDirective } from '@shared/directives/type-template/type-tem
 export class ContractFormComponent implements OnInit {
   @Input() contract: Contract;
   public settings: ContractSettings;
+  public contactList: CompanyContact[];
+  public truckerList: CompanyContact[];
 
   @ViewChildren(TypeTemplateDirective) public versionTemplates: QueryList<TemplateRef<any>>;
 
   constructor(
     private db: Firestore,
-    private session: SessionInfo
+    private session: SessionInfo,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit() {
     ContractSettings.getDocument(this.db, this.session.getCompany()).then(result => {
       this.settings = result;
+    });
+
+    Company.getCompany(this.db, this.session.getCompany()).then(val => {
+      this.contactList = val.contactList.sort((a, b) =>{
+          var nameA = a.name.toUpperCase()
+          var nameB = b.name.toUpperCase()
+
+          if(nameA < nameB){
+            return -1;
+          }
+          if(nameA > nameB){
+            return 1;
+          }
+
+          return 0;
+        });
+      this.truckerList = this.contactList.filter(c => !c.isClient);
+    });
+  }
+
+  openClientSelect() {
+    const dialogRef = this.dialog.open(SelectClientComponent, {
+      width: '600px',
+      data: this.contactList
+    });
+
+
+    lastValueFrom(dialogRef.afterClosed()).then(result => {
+      if(!result) return;
+
+      Contact.getDoc(this.db, this.session.getCompany(), result[0].id).then(client => {
+        //this.selectedClient = client;
+        this.contract.clientInfo = Contract.clientInfo(client);
+        this.contract.clientName = client.name;
+      });
+    });
+  }
+
+  openTicketClientSelect() { 
+    const dialogRef = this.dialog.open(SelectClientComponent, {
+      width: '600px',
+      data: this.contactList
+    });
+
+    lastValueFrom(dialogRef.afterClosed()).then(result => {
+      Contact.getDoc(this.db, this.session.getCompany(), result[0].id).then(client => {
+        //this.ticketClient = client;
+        this.contract.clientTicketInfo = Contract.clientInfo(client);
+        this.contract.clientTicketInfo.name = client.name;
+      });
     });
   }
 }
