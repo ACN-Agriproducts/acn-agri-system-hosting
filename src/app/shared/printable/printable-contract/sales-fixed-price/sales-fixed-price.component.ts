d96a5d92522b1f3faf476228c58ec5f34ae43031
rubpu <rubpuga@gmail.com>
2023-05-17 13:40:40 -0500

    Implemented new Directive

--- src/app/shared/printable/printable-contract/sales-fixed-price/sales-fixed-price.component.ts
+++ src/app/shared/printable/printable-contract/sales-fixed-price/sales-fixed-price.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
 import { Contract } from '@shared/classes/contract';
 import { PrintableContractUtilitiesService } from '../printable-contract-utilities.service';
+import { FocusedFieldDirective } from '../printable-contract.component';
 
 @Component({
   selector: 'contract-sales-fixed-price',
@@ -10,13 +11,20 @@ import { PrintableContractUtilitiesService } from '../printable-contract-utiliti
     '../contract-styles.scss'
   ],
 })
-export class SalesFixedPriceComponent implements OnInit {
+export class SalesFixedPriceComponent implements OnInit, OnChanges {
   @Input() contractForm: Contract;
   @Input() focusedField: string;
+  @ViewChildren(FocusedFieldDirective) fieldsList: QueryList<FocusedFieldDirective>;
   readonly contractType: string = 'deVenta_precioFijo';
 
   constructor(public utils: PrintableContractUtilitiesService) { }
 
   ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+      if(changes['focusedField'] && this.focusedField) {
+        this.fieldsList?.find(ff => ff.fieldName == this.focusedField)?.el.nativeElement.scrollIntoView({behavior: 'smooth', block: 'center'});
+      }
+  }
 
 }
