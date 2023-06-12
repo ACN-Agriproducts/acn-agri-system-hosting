bac36da1631ade0464bea4277962e42555aef5e8
Pugster01 <rubpuga@gmail.com>
2023-06-12 09:36:11 -0500

    Created includes pipe

--- src/app/core/directive/directives-core.module.ts
+++ src/app/core/directive/directives-core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CursorDirective } from './cursor.directive';
 import { PositiveDirective } from './positive/positive.directive';
+import { IncludesPipe } from './includes/includes.pipe';
 @NgModule({
   imports: [CommonModule],
-  declarations: [CursorDirective, PositiveDirective],
-  exports: [CursorDirective, PositiveDirective]
+  declarations: [CursorDirective, PositiveDirective, IncludesPipe],
+  exports: [CursorDirective, PositiveDirective, IncludesPipe]
 })
 export class DirectivesCoreModule { }
