7a398a2355ca08d0ee89b933e6f523beef9a10e3
Jeddie <eduardoreyna0625@hotmail.com>
2023-06-13 17:11:52 -0500

    Added option to reorder elements in discount table

--- src/app/pages/inventory/product/components/discount-table/discount-table.component.ts
+++ src/app/pages/inventory/product/components/discount-table/discount-table.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { DiscountTable } from '@shared/classes/discount-tables';
 
@@ -8,11 +9,15 @@ import { DiscountTable } from '@shared/classes/discount-tables';
 })
 export class DiscountTableComponent implements OnInit {
   @Input() table: DiscountTable;
+  
+  public reordering: boolean = false;
+  public removing: boolean = false;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
 
+  drop(event: CdkDragDrop<any>) {
+    moveItemInArray(this.table.data, event.previousIndex, event.currentIndex);
   }
-
 }
