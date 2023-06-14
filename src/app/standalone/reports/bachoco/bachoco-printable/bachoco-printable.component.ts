230ae094ba892ff96ffad00b386a971f117cef05
rubpu <rubpuga@gmail.com>
2023-06-14 10:46:49 -0500

    Bachoco report

--- src/app/standalone/reports/bachoco/bachoco-printable/bachoco-printable.component.ts
+++ src/app/standalone/reports/bachoco/bachoco-printable/bachoco-printable.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contract } from '@shared/classes/contract';
+import { Ticket } from '@shared/classes/ticket';
 
 @Component({
   selector: 'app-bachoco-printable',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './bachoco-printable.component.html',
   styleUrls: ['./bachoco-printable.component.scss'],
 })
 export class BachocoPrintableComponent implements OnInit {
+  @Input() contract: Contract;
+  @Input() tickets: Ticket[];
 
   constructor() { }
 
