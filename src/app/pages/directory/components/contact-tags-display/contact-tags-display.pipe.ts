b7c5ca15d7567685f1cec2034b2b188ff16e9cff
Pugster01 <rubpuga@gmail.com>
2023-06-29 10:15:45 -0500

    contact display tags pipe init

---
+++ src/app/pages/directory/components/contact-tags-display/contact-tags-display.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
+import { Contact } from '@shared/classes/contact';
+
+@Pipe({
+  name: 'contactTagsDisplay'
+})
+export class ContactTagsDisplayPipe implements PipeTransform {
+  constructor(private transloco: TranslocoService) {}
+
+  transform(contact: Contact, ...args: unknown[]): string {
+    return contact.getType().map(t => this.transloco.translate(`directory.${t}`)).join(' / ');
+  }
+
+}
