f657f709a3eec6d38392cf20b25b1f5dc39478cd
rubpu <rubpuga@gmail.com>
2023-08-16 10:53:39 -0500

    trucker field dialog init

---
+++ src/app/pages/directory/trucker-fields.dialog/trucker-fields.dialog.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trucker-fields.dialog',
+  templateUrl: './trucker-fields.dialog.html',
+  styleUrls: ['./trucker-fields.dialog.scss'],
+})
+export class TruckerFieldsDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
