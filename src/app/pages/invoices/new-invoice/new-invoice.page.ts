e9b4fa14b90d3e98ef819a819081b7df5c506d09
rubpu <rubpuga@gmail.com>
2023-08-22 08:50:25 -0500

    new ticket query for new invoice page

--- src/app/pages/invoices/new-invoice/new-invoice.page.ts
+++ src/app/pages/invoices/new-invoice/new-invoice.page.ts
@@ -20,7 +20,7 @@ export class NewInvoicePage implements OnInit {
 
   ngOnInit() {
     const today = new Date();
-    const requestDate = new Date(2023, 0, 0) //new Date(today.getFullYear(), today.getMonth(), today.getDate() - 14);
+    const requestDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 14);
     this.selectedTickets = new Set<Ticket>;
 
     this.ticketsPromise = Ticket.getTickets(
