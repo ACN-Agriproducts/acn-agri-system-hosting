22cb1b664d6b6e6a7c430d2ba4fc9fc00a04906f
rubpu <rubpuga@gmail.com>
2023-08-25 07:34:19 -0500

    start of contract report page

--- src/app/pages/reports/contract-reports/contract-reports.page.ts
+++ src/app/pages/reports/contract-reports/contract-reports.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { MAT_DATE_RANGE_SELECTION_STRATEGY } from '@angular/material/datepicker';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { WeekRangeSelectionStrategy } from '@pages/inventory/storage-logs/storage-logs.page';
+import { ContractSettings } from '@shared/classes/contract-settings';
 
 @Component({
   selector: 'app-contract-reports',
   templateUrl: './contract-reports.page.html',
   styleUrls: ['./contract-reports.page.scss'],
+  providers: [
+    {
+      provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
+      useClass: WeekRangeSelectionStrategy
+    }
+  ]
 })
 export class ContractReportsPage implements OnInit {
+  contractSettings: Promise<ContractSettings>;
+  startDate: Date;
+  endDate: Date;
 
-  constructor() { }
+  selectedContractTypes: string[];
+
+  constructor(
+    private db: Firestore,
+    private session: SessionInfo
+  ) { }
 
   ngOnInit() {
+    this.contractSettings = ContractSettings.getDocument(this.db, this.session.getCompany());
+
+    this.selectedContractTypes = [];
   }
 
 }
