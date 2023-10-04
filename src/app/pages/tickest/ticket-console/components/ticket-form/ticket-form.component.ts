6bee6f0d991068ca7d5a6889c05c6c5db7bf5ca8
rubpu <rubpuga@gmail.com>
2023-10-04 13:30:36 -0500

    moving get calls to correct layer

--- src/app/pages/tickest/ticket-console/components/ticket-form/ticket-form.component.ts
+++ src/app/pages/tickest/ticket-console/components/ticket-form/ticket-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from '@shared/classes/contact';
+import { Contract } from '@shared/classes/contract';
+import { Ticket } from '@shared/classes/ticket';
 
 @Component({
   selector: 'app-ticket-form',
@@ -6,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-form.component.scss'],
 })
 export class TicketFormComponent implements OnInit {
+  @Input() ticket: Ticket;
+  @Input() openContracts: Contract[];
+  @Input() transportList: Contact[];
 
-  constructor() { }
+  constructor( ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
--- src/app/pages/tickest/ticket-console/ticket-console.page.ts
+++ src/app/pages/tickest/ticket-console/ticket-console.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, query, where } from '@angular/fire/firestore';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { Contact } from '@shared/classes/contact';
+import { Contract } from '@shared/classes/contract';
+import { Ticket } from '@shared/classes/ticket';
 
 @Component({
   selector: 'app-ticket-console',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-console.page.scss'],
 })
 export class TicketConsolePage implements OnInit {
+  tickets: Ticket[];
+  openContracts: Contract[] = [];
+  transportList: Promise<Contact[]>;
+  ticketIndex: number = 0;
 
-  constructor() { }
+  constructor(
+    private session: SessionInfo,
+    private db: Firestore,
+  ) { }
 
   ngOnInit() {
+    Contract.onSnapshot(
+      query(Contract.getCollectionReference(this.db, this.session.getCompany()),
+        where('status', '==', 'active'), where('plants', 'array-contains', this.session.getPlant())),
+      this.openContracts,
+      snap => console.log("Contracts", snap.docs.length)
+    );
+
+    this.transportList = Contact.getList(this.db, this.session.getCompany(), where('tags', 'array-contains', 'trucker'));
+    this.transportList.then(next => console.log("transportList", next.length));
   }
 
 }
