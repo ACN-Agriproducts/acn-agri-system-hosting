130d8cc213a4ed73c5151630159f70859d1b480d
Jeddie <eduardoreyna0625@hotmail.com>
2023-10-23 10:27:02 -0500

    Improved usability when selecting tickets for liquidation

--- src/app/pages/contracts/contract-info/set-liquidation/set-liquidation.module.ts
+++ src/app/pages/contracts/contract-info/set-liquidation/set-liquidation.module.ts
@@ -12,6 +12,7 @@ import { TicketDiscountTableComponent } from '../set-liquidation/components/tick
 import { NgxPrintModule } from 'ngx-print';
 import { LiquidationPrintablesModule } from 'src/app/modules/liquidation-printables/liquidation-printables.module';
 import { SharedModule } from "../../../../shared/shared.module";
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
     declarations: [
@@ -27,6 +28,7 @@ import { SharedModule } from "../../../../shared/shared.module";
         NgxPrintModule,
         LiquidationPrintablesModule,
         SharedModule,
+        MatRippleModule
     ]
 })
 export class SetLiquidationPageModule {}
