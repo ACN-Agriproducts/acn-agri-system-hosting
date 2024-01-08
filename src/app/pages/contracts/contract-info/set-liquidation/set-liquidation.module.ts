96f2089356bf4dd9a24aaae97b869fa0f919634f
Pugster01 <rubpuga@gmail.com>
2024-01-08 10:13:15 -0600

    remove unsued component

--- src/app/pages/contracts/contract-info/set-liquidation/set-liquidation.module.ts
+++ src/app/pages/contracts/contract-info/set-liquidation/set-liquidation.module.ts
@@ -8,7 +8,6 @@ import { NewLiquidationPageRoutingModule } from './set-liquidation-routing.modul
 
 import { SetLiquidationPage } from './set-liquidation.page';
 import { CoreModule } from '@core/core.module';
-import { TicketDiscountTableComponent } from '../set-liquidation/components/ticket-discount-table/ticket-discount-table.component';
 import { NgxPrintModule } from 'ngx-print';
 import { LiquidationPrintablesModule } from 'src/app/modules/liquidation-printables/liquidation-printables.module';
 import { SharedModule } from "../../../../shared/shared.module";
@@ -17,7 +16,6 @@ import { MatRippleModule } from '@angular/material/core';
 @NgModule({
     declarations: [
         SetLiquidationPage,
-        TicketDiscountTableComponent,
     ],
     imports: [
         CommonModule,
