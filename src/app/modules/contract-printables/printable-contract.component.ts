80e7d54c819862de1e2e286c4e9097fca0649236
Jeddie <eduardoreyna0625@hotmail.com>
2024-02-21 16:50:42 -0600

    Updates to printable-contract component

--- src/app/modules/contract-printables/printable-contract.component.ts
+++ src/app/modules/contract-printables/printable-contract.component.ts
@@ -1,7 +1,5 @@
-import { Component, Directive, ElementRef, EventEmitter, HostBinding, Input, OnInit, Output, QueryList, TemplateRef, ViewChildren } from '@angular/core';
+import { Component, Directive, ElementRef, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
 import { Contract } from '@shared/classes/contract';
-import { TypeTemplateDirective } from '@core/directive/type-template/type-template.directive';
-import { BehaviorSubject, filter, map, Observable } from 'rxjs';
 import { ContractSettings } from '@shared/classes/contract-settings';
 import { Company } from '@shared/classes/company';
 import { SessionInfo } from '@core/services/session-info/session-info.service';
@@ -13,24 +11,13 @@ import { Firestore } from '@angular/fire/firestore';
   styleUrls: ['./printable-contract.component.scss'],
 })
 export class PrintableContractComponent implements OnInit {
-  @ViewChildren(TypeTemplateDirective) private versionTemplates: QueryList<TypeTemplateDirective>;
-
-  @Input("version") set version(newVersion: string) {
-    this.version$.next(newVersion);
-  }
   @Input() contract: Contract;
   @Input() focusedField: string;
 
-  @Input() templateVersion: string;
+  @Input() template: string;
   public company: Promise<Company>;
 
   // @Output() contractTypesListEmitter = new EventEmitter<Map<string, string>>();
-
-  public version$: BehaviorSubject<string> = new BehaviorSubject<string>(null);
-  public template$: Observable<TemplateRef<any>> = this.version$.pipe(
-    filter(() => !!this.versionTemplates),
-    map(version => this.versionTemplates.find(template => template.typeTemplate === (version ?? this.contract.type))?.templateRef)
-  );
 
   public settings: ContractSettings;
 
@@ -48,9 +35,6 @@ export class PrintableContractComponent implements OnInit {
     this.company = Company.getCompany(this.db, this.session.getCompany());
   }
 
-  ngAfterViewInit() {
-    this.version$.next(this.version$.getValue());
-  }
 }
 
 @Directive({
