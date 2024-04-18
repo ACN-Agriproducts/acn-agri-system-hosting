ed8f41d4d52e05dcfa899c15250fa5054baf9870
rubpu <rubpuga@gmail.com>
2024-04-18 09:39:56 -0500

    Added dynamic logo to printable ticket

--- src/app/shared/printable/printable-ticket/printable-ticket.component.ts
+++ src/app/shared/printable/printable-ticket/printable-ticket.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { SessionInfo } from '@core/services/session-info/session-info.service';
+import { Company } from '@shared/classes/company';
 import { Contact } from '@shared/classes/contact';
 import { Contract } from '@shared/classes/contract';
 import { Ticket } from '@shared/classes/ticket';
@@ -9,12 +12,23 @@ import { Ticket } from '@shared/classes/ticket';
   templateUrl: './printable-ticket.component.html',
   styleUrls: ['./printable-ticket.component.scss']
 })
-export class PrintableTicketComponent {
+export class PrintableTicketComponent implements OnInit {
   @Input() ticket: Ticket;
   @Input() contract: Contract;
   @Input() transport: Contact;
   @Input() client: Contact;
 
-  constructor() {}
+  company: Promise<Company>;
+  logoURL = '';
+
+  constructor(
+    private db: Firestore,
+    private session: SessionInfo,
+  ) {}
+
+  ngOnInit() {
+    this.company = this.session.getCompanyObject();
+    this.company.then(async doc => this.logoURL = await doc.getLogoURL(this.db));
+  }
 }
  
