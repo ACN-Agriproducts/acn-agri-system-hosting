0bacc581a32996cc4dc8dc87d6e326bef0b34084
Jeddie <eduardoreyna0625@hotmail.com>
2024-06-28 14:56:44 -0500

    Added translations for warehouse receipt pages and components

---
+++ src/app/core/pipes/translocoNested/transloco-nested.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TranslocoNestedPipe } from './transloco-nested.pipe';
+
+describe('TranslocoNestedPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TranslocoNestedPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
