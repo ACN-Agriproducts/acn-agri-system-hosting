21cd926fd29e6c2fa81bec12f852911644ea263d
Pugster01 <rubpuga@gmail.com>
2024-07-03 21:10:53 -0500

    implemented client account card

--- src/app/pages/reports/contract-payment-reports/components/client-card/client-card.component.ts
+++ src/app/pages/reports/contract-payment-reports/components/client-card/client-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ClientAccount } from '../../contract-payment-reports.page';
 
 @Component({
   selector: 'app-client-card',
@@ -6,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-card.component.scss'],
 })
 export class ClientCardComponent implements OnInit {
+  @Input() data: ClientAccount; 
+  public tableData: TableData[];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const tempTableData: TableData[] = [];
 
+    if(this.data.purchase.paid + this.data.purchase.pendingToPay > 0) {
+      tempTableData.push({
+        name: "Purchase",
+        series: [{
+          name: "Paid",
+          value: this.data.purchase.paid
+        }, {
+          name: "Pending",
+          value: this.data.purchase.pendingToPay
+        }]
+      });
+    }
+
+    if(this.data.sale.paid + this.data.sale.pendingToPay > 0) {
+      tempTableData.push({
+        name: "Sales",
+        series: [{
+          name: "Paid",
+          value: this.data.sale.paid
+        }, {
+          name: "Pending",
+          value: this.data.sale.pendingToPay
+        }]
+      });
+    }
+
+    this.tableData = tempTableData;
+  }
+
+  openContractsDialog() {
+    
+  }
+
+}
+
+interface TableData {
+  name: string,
+  series: {
+    name: string,
+    value: number;
+  }[]
 }
