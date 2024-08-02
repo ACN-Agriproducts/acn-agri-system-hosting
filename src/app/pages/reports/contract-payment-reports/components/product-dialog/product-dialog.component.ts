43f9cc2729a08e3f792890b38e299e31ca05c82b
rubpu <rubpuga@gmail.com>
2024-08-02 10:40:39 -0500

    product dialog init

---
+++ src/app/pages/reports/contract-payment-reports/components/product-dialog/product-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Contract } from '@shared/classes/contract';
+import { ProductAccount } from '../../contract-payment-reports.page';
+
+@Component({
+  selector: 'app-product-dialog',
+  templateUrl: './product-dialog.component.html',
+  styleUrls: ['./product-dialog.component.scss'],
+})
+export class ProductDialogComponent implements OnInit {
+  purchaseContracts: Contract[];
+  saleContracts: Contract[];
+  
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: ProductAccount
+  ) { }
+
+  ngOnInit() {
+    this.purchaseContracts = this.data.contracts.filter(c => c.tags.includes('purchase'));
+    this.saleContracts = this.data.contracts.filter(c => c.tags.includes('sale'));
+  }
+}
