501e3e03ce0aabb9d3dcfa3e8f1f252ffe7f16b7
rubpu <rubpuga@gmail.com>
2024-08-05 08:35:58 -0500

    Status chip component init

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -23,6 +23,7 @@ import { TranslatePipe } from './pipes/translate/translate.pipe';
 import { IncludesPipe } from './pipes/includes/includes.pipe';
 import { TranslocoModule } from '@ngneat/transloco';
 import { TranslocoNestedPipe } from './pipes/translocoNested/transloco-nested.pipe';
+import { StatusChipComponent } from './components/status-chip/status-chip/status-chip.component';
 
 @NgModule({
   imports: [
@@ -78,6 +79,7 @@ import { TranslocoNestedPipe } from './pipes/translocoNested/transloco-nested.pi
     TranslatePipe,
     IncludesPipe,
     TranslocoNestedPipe,
+    StatusChipComponent
   ]
 })
 export class CoreModule { }
