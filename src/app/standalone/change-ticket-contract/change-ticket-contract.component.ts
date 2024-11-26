84d21c77e20e23ec8e64db6973f3002b0c8edf58
Jeddie <eduardoreyna0625@hotmail.com>
2024-11-26 15:50:02 -0600

    Updated new contract form field to display possible contracts in groups

--- src/app/standalone/change-ticket-contract/change-ticket-contract.component.ts
+++ src/app/standalone/change-ticket-contract/change-ticket-contract.component.ts
@@ -25,7 +25,8 @@ import { Ticket } from '@shared/classes/ticket';
 })
 export class ChangeTicketContractComponent implements OnInit {
   public contract: Contract;
-  public possibleContracts: Promise<Contract[]>;
+  // public possibleContracts: Promise<Contract[]>;
+  public possibleContracts: Promise<{ client: Contract[], other: Contract[] }>;
   public newContract: Contract;
   public displayUnit: units;
   public language: string;
@@ -41,15 +42,39 @@ export class ChangeTicketContractComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.data);
+    // this.possibleContracts = this.data.getContract(this.db).then(ticketContract => {
+    //   this.contract = ticketContract;
+    //   this.data.net.defineBushels(ticketContract.productInfo);
+    //   return Contract.getContracts(
+    //     this.db, this.session.getCompany(), ticketContract.type,
+    //     where('client', '==', ticketContract.client),
+    //     where('status', '==', 'active'),
+    //     where('product', '==', ticketContract.product),
+    //     where(documentId(), "!=", ticketContract.ref.id));
+    // });
+
     this.possibleContracts = this.data.getContract(this.db).then(ticketContract => {
       this.contract = ticketContract;
       this.data.net.defineBushels(ticketContract.productInfo);
       return Contract.getContracts(
         this.db, this.session.getCompany(), ticketContract.type,
-        where('client', '==', ticketContract.client),
         where('status', '==', 'active'),
         where('product', '==', ticketContract.product),
         where(documentId(), "!=", ticketContract.ref.id));
+    })
+    .then(possibleContracts => {
+      const clientContracts = [];
+      const otherContracts = [];
+
+      for (const contract of possibleContracts) {
+        if (contract.client.id == this.contract.client.id) clientContracts.push(contract);
+        else otherContracts.push(contract);
+      }
+
+      return {
+        client: clientContracts,
+        other: otherContracts
+      }
     });
 
     this.displayUnit = this.session.getDisplayUnit();
