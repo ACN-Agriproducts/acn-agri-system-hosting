f0da63f7e0a730b9e9dd322fe68c6796b0e9892f
Jeddie <eduardoreyna0625@hotmail.com>
2025-11-03 16:30:33 -0600

    Added commas to mass display pipe to more easily recognize values

--- src/app/core/pipes/mass/mass-display.pipe.ts
+++ src/app/core/pipes/mass/mass-display.pipe.ts
@@ -6,9 +6,12 @@ import { Mass, units } from '@shared/classes/mass';
 })
 export class MassDisplayPipe implements PipeTransform {
 
-  transform(value: Mass, decimalDigits: number = 2, unit?: units, ...others: any[]): unknown {
+  transform(value: Mass, decimalDigits: number = 2, unit?: units, ...others: any[]): string {
     unit ??= value.defaultUnits;
-    return `${value.getMassInUnit(unit).toFixed(decimalDigits)} ${unit}`;
+    return `${value.getMassInUnit(unit).toLocaleString('en-US', {
+      minimumFractionDigits: decimalDigits,
+      maximumFractionDigits: decimalDigits
+    })} ${unit}`;
   }
 
 }
