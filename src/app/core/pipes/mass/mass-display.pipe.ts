2eb2bec98ec4e786972fcc743006375db430fb35
Jeddie <eduardoreyna0625@hotmail.com>
2025-11-11 15:29:56 -0600

    Added option for masking falsy values to the mass display pipe

--- src/app/core/pipes/mass/mass-display.pipe.ts
+++ src/app/core/pipes/mass/mass-display.pipe.ts
@@ -9,9 +9,11 @@ import { Mass, units } from '@shared/classes/mass';
 })
 export class MassDisplayPipe implements PipeTransform {
 
-  transform(value: Mass, decimalDigits: number = 2, unit?: units, ...others: any[]): string {
-    unit ??= value.defaultUnits;
-    return `${value.getMassInUnit(unit).toLocaleString('en-US', {
+  transform(value: Mass, decimalDigits: number = 2, unit: units = value.defaultUnits, maskFalsyValues: boolean = false, ...others: any[]): string {
+    let massInUnit: number | string = value.getMassInUnit(unit);
+    massInUnit = maskFalsyValues ? (massInUnit || '--') : massInUnit;
+
+    return `${massInUnit.toLocaleString('en-US', {
       minimumFractionDigits: decimalDigits,
       maximumFractionDigits: decimalDigits
     })} ${unit}`;
