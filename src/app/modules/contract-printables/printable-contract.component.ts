bdd56a80fadd74b6ac5c8f7a85cacbe1d0cb6621
Jeddie <eduardoreyna0625@hotmail.com>
2026-07-10 16:12:05 -0500

    Updated printable contract component to use directive to refer to components in the template

--- src/app/modules/contract-printables/printable-contract.component.ts
+++ src/app/modules/contract-printables/printable-contract.component.ts
@@ -4,8 +4,6 @@ import { ContractSettings } from '@shared/classes/contract-settings';
 import { Company } from '@shared/classes/company';
 import { SessionInfo } from '@core/services/session-info/session-info.service';
 import { Firestore } from '@angular/fire/firestore';
-import { TypeTemplateDirective } from '@core/directive/type-template/type-template.directive';
-import { BehaviorSubject, Observable, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-printable-contract',
@@ -13,21 +11,9 @@ import { BehaviorSubject, Observable, filter, map } from 'rxjs';
   styleUrls: ['./printable-contract.component.scss'],
 })
 export class PrintableContractComponent implements OnInit {
-  @ViewChildren(TypeTemplateDirective) private versionTemplates: QueryList<TypeTemplateDirective>;
-
-  @Input("version") set version(newVersion: string) {
-    this.version$.next(newVersion);
-  }
   @Input() contract: Contract;
   @Input() focusedField: string;
-  
-  // @Output() contractTypesListEmitter = new EventEmitter<Map<string, string>>();
-
-  public version$: BehaviorSubject<string> = new BehaviorSubject<string>(null);
-  public template$: Observable<TemplateRef<any>> = this.version$.pipe(
-    filter(() => !!this.versionTemplates),
-    map(version => this.versionTemplates.find(template => template.typeTemplate === (version ?? this.contract.type))?.templateRef)
-  );
+  @Input() version: string;
   
   public settings: ContractSettings;
   public company: Promise<Company>;
@@ -40,14 +26,9 @@ export class PrintableContractComponent implements OnInit {
   ngOnInit() {
     ContractSettings.getContractDoc(this.contract).then(result => {
       this.settings = result;
-      // this.contractTypesListEmitter.emit(new Map(Object.entries(result.contractTypes)));
     });
 
     this.company = Company.getCompany(this.db, this.session.getCompany());
-  }
-
-  ngAfterViewInit() {
-    this.version$.next(this.version$.getValue());
   }
 
 }
